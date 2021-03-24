059cb9acbd6f3cb20e006932aeab176a3c463130
shuigedeng <981376577@qq.com>
2021-03-24 10:21:59 +0800

    [taotao-cloud-project-1092] update openresty config

--- taotao-cloud-config/src/main/java/com/taotao/cloud/processor/taotao-bigdata/openresty1/19/3/1/script/spilt-access-log.sh
+++ taotao-cloud-config/src/main/java/com/taotao/cloud/processor/taotao-bigdata/openresty1/19/3/1/script/spilt-access-log.sh
@@ -16,12 +16,12 @@ usage(){
 
 default(){
   echo "use default"
-  echo "log_file=/opt/openresty/data/taotao-cloud.access.log"
-  echo "data_dir=/opt/openresty/data/logs/"
+  echo "log_file=/opt/openresty/weblog/taotao-cloud.access.log"
+  echo "data_dir=/opt/openresty/weblog/data/"
   echo "pid_file=/opt/openresty/pid/nginx.pid"
 
-  log_file="/opt/openresty/taotao-bigdata/logs/taotao-cloud.access.log"
-  data_dir="/opt/openresty/taotao-bigdata/logs/data/"
+  log_file="/opt/openresty/weblog/taotao-cloud.access.log"
+  data_dir="/opt/openresty/weblog/data/"
   pid_file="/opt/openresty/pid/nginx.pid"
 }
 
