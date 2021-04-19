5d9d3a9617726f69a38d8ff37e1912d81f1cb187
shuigedeng <981376577@qq.com>
2021-04-19 17:49:46 +0800

    [taotao-cloud-project-1128] delete mini mall back

--- taotao-cloud-web/taotao-cloud-mall-back/config/tailwind/h5.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  purge: {
-    enabled: true,
-    content: ['./src/**/*.{js,jsx,ts,tsx,vue}'],
-    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-  },
-};
