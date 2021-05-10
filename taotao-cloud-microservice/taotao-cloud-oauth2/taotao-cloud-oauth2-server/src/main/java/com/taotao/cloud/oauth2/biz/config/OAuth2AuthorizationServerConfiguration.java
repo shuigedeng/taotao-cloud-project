7a91ffbfeb6697c9a1cd209c7bf159a98d098bed
shuigedeng <981376577@qq.com>
2021-05-10 17:44:40 +0800

    [taotao-cloud-project-1140] update taotao cloud oauth2 client

--- taotao-cloud-microservice/taotao-cloud-oauth2/taotao-cloud-oauth2-server/src/main/java/com/taotao/cloud/oauth2/biz/config/OAuth2AuthorizationServerConfiguration.java
+++ taotao-cloud-microservice/taotao-cloud-oauth2/taotao-cloud-oauth2-server/src/main/java/com/taotao/cloud/oauth2/biz/config/OAuth2AuthorizationServerConfiguration.java
@@ -5,7 +5,6 @@ import com.taotao.cloud.oauth2.biz.social.UserDetailTypeOAuthUserService;
 import com.taotao.cloud.oauth2.biz.social.converter.CustomMapOAuth2AccessTokenResponseConverter;
 import com.taotao.cloud.oauth2.biz.social.converter.CustomMappingJackson2HttpMessageConverter;
 import com.taotao.cloud.oauth2.biz.social.converter.CustomOAuth2AuthorizationCodeGrantRequestEntityConverter;
-import com.taotao.cloud.oauth2.biz.social.converter.CustomOAuth2AuthorizationRequestResolver;
 import com.taotao.cloud.oauth2.biz.social.converter.CustomOAuth2UserRequestEntityConverter;
 import com.taotao.cloud.oauth2.biz.social.user.GitHubOAuth2User;
 import com.taotao.cloud.oauth2.biz.social.user.GiteeOAuth2User;
@@ -15,6 +14,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+import javax.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
@@ -27,11 +28,16 @@ import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationC
 import org.springframework.security.oauth2.client.http.OAuth2ErrorResponseErrorHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.userinfo.CustomUserTypesOAuth2UserService;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestRedirectFilter;
+import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames;
 import org.springframework.security.oauth2.core.http.converter.OAuth2AccessTokenResponseHttpMessageConverter;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Slf4j
 @Component
@@ -51,15 +57,16 @@ public class OAuth2AuthorizationServerConfiguration extends WebSecurityConfigure
 			.invalidateHttpSession(true)
 			.and()
 			.oauth2Login(config -> config
-					// 认证成功后的处理器
-					.successHandler(new Oauth2AuthenticationSuccessHandler())
-					// 认证失败后的处理器
-//				.failureHandler(new Oauth2AuthenticationFailureHandler())
-//				.loginPage("/login")
-					.tokenEndpoint(this::tokenEndpoint)
-					.userInfoEndpoint(this::userInfoEndpoint)
-					.authorizationEndpoint(this::authorizationEndpoint)
+				// 认证成功后的处理器
+				.successHandler(new CustomOAuth2AuthenticationSuccessHandler())
+				// 认证失败后的处理器
+				.failureHandler(new Oauth2AuthenticationFailureHandler())
+				.loginPage("/login")
+				.tokenEndpoint(this::tokenEndpoint)
+				.userInfoEndpoint(this::userInfoEndpoint)
+				.authorizationEndpoint(this::authorizationEndpoint)
 			)
+			.logout(config -> config.logoutSuccessHandler(new CustomLogoutSuccessHandler()))
 			.authorizeRequests()
 			.antMatchers(
 				"/login",
@@ -83,9 +90,27 @@ public class OAuth2AuthorizationServerConfiguration extends WebSecurityConfigure
 	private void authorizationEndpoint(
 		OAuth2LoginConfigurer<HttpSecurity>.AuthorizationEndpointConfig authorization) {
 
-		authorization.authorizationRequestResolver(
-			new CustomOAuth2AuthorizationRequestResolver(clientRegistrationRepository,
-				OAuth2AuthorizationRequestRedirectFilter.DEFAULT_AUTHORIZATION_REQUEST_BASE_URI));
+		// 处理 /{registrationId}/oauth2/authorization/{registrationId}
+		// 由于网关服务, 跳转需加前缀 spring.application.name, 将 name 和 registrationId 取名相同解决
+		DefaultOAuth2AuthorizationRequestResolver resolver = new DefaultOAuth2AuthorizationRequestResolver(
+			clientRegistrationRepository,
+			OAuth2AuthorizationRequestRedirectFilter.DEFAULT_AUTHORIZATION_REQUEST_BASE_URI);
+
+		// 保存 redirect_url 参数
+		resolver.setAuthorizationRequestCustomizer(build -> build.attributes(attributesConsumer -> {
+			ServletRequestAttributes attrs = (ServletRequestAttributes) RequestContextHolder
+				.getRequestAttributes();
+			HttpServletRequest request = Objects.requireNonNull(attrs).getRequest();
+			String redirectUri = request.getParameter(OAuth2ParameterNames.REDIRECT_URI);
+			// todo 可以再这里做判断redirectUri,走白名单, 或者写死redirectUri
+			assert StringUtils.isEmpty(redirectUri);
+			attributesConsumer.put(OAuth2ParameterNames.REDIRECT_URI, redirectUri);
+		}));
+		authorization.authorizationRequestResolver(resolver);
+
+		// 恢复 redirect_url 参数
+		authorization
+			.authorizationRequestRepository(new CustomOAuth2AuthorizationRequestRepository());
 	}
 
 	// 解决第三方授权登录兼容性问题
