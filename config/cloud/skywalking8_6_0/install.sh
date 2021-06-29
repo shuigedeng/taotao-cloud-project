2b3cdfa6eecd838c3e6b5e7e086692673ee557c2
shuigedeng <981376577@qq.com>
2021-06-29 14:04:27 +0800

    [taotao-cloud-project-1174] add skywalking and sleuth zipkin config update bootstrap-dev.yml

--- config/cloud/skywalking8_6_0/install.sh
+++ config/cloud/skywalking8_6_0/install.sh
@@ -11,13 +11,17 @@ tar -zxvf apache-skywalking-apm-es7-8.6.0.tar.gz
 
 # 4、修改配置文件
 vim skywalking8.6.0/config/application.yml
-#修改h2 为mysql
+
 storage:
+  #修改h2 为mysql
   selector: ${SW_STORAGE:mysql}
   mysql:
     properties:
-      jdbcUrl: ${SW_JDBC_URL:"jdbc:mysql://127.0.0.1:3306/taotao-cloud-skywalking"}
+      #修改jdbcUrl
+      jdbcUrl: ${SW_JDBC_URL:"jdbc:mysql://192.168.1.10:3306/taotao-cloud-skywalking"}
+      #修改user
       dataSource.user: ${SW_DATA_SOURCE_USER:root}
+      #修改password
       dataSource.password: ${SW_DATA_SOURCE_PASSWORD:123456}
       dataSource.cachePrepStmts: ${SW_DATA_SOURCE_CACHE_PREP_STMTS:true}
       dataSource.prepStmtCacheSize: ${SW_DATA_SOURCE_PREP_STMT_CACHE_SQL_SIZE:250}
@@ -26,6 +30,31 @@ storage:
     metadataQueryMaxSize: ${SW_STORAGE_MYSQL_QUERY_MAX_SIZE:5000}
     maxSizeOfArrayColumn: ${SW_STORAGE_MAX_SIZE_OF_ARRAY_COLUMN:20}
     numOfSearchableValuesPerTag: ${SW_STORAGE_NUM_OF_SEARCHABLE_VALUES_PER_TAG:2}
+
+receiver-sharing-server:
+  selector: ${SW_RECEIVER_SHARING_SERVER:default}
+  default:
+    # For Jetty server
+    restHost: ${SW_RECEIVER_SHARING_REST_HOST:0.0.0.0}
+    restPort: ${SW_RECEIVER_SHARING_REST_PORT:0}
+    restContextPath: ${SW_RECEIVER_SHARING_REST_CONTEXT_PATH:/}
+    restMinThreads: ${SW_RECEIVER_SHARING_JETTY_MIN_THREADS:1}
+    restMaxThreads: ${SW_RECEIVER_SHARING_JETTY_MAX_THREADS:200}
+    restIdleTimeOut: ${SW_RECEIVER_SHARING_JETTY_IDLE_TIMEOUT:30000}
+    restAcceptorPriorityDelta: ${SW_RECEIVER_SHARING_JETTY_DELTA:0}
+    restAcceptQueueSize: ${SW_RECEIVER_SHARING_JETTY_QUEUE_SIZE:0}
+    # For gRPC server
+    gRPCHost: ${SW_RECEIVER_GRPC_HOST:0.0.0.0}
+    gRPCPort: ${SW_RECEIVER_GRPC_PORT:0}
+    maxConcurrentCallsPerConnection: ${SW_RECEIVER_GRPC_MAX_CONCURRENT_CALL:0}
+    maxMessageSize: ${SW_RECEIVER_GRPC_MAX_MESSAGE_SIZE:0}
+    gRPCThreadPoolQueueSize: ${SW_RECEIVER_GRPC_POOL_QUEUE_SIZE:0}
+    gRPCThreadPoolSize: ${SW_RECEIVER_GRPC_THREAD_POOL_SIZE:0}
+    gRPCSslEnabled: ${SW_RECEIVER_GRPC_SSL_ENABLED:false}
+    gRPCSslKeyPath: ${SW_RECEIVER_GRPC_SSL_KEY_PATH:""}
+    gRPCSslCertChainPath: ${SW_RECEIVER_GRPC_SSL_CERT_CHAIN_PATH:""}
+    #修改authentication
+    authentication: ${SW_AUTHENTICATION:"taotao-cloud"}
 
 # 5、下载mysql驱动包到 /opt/skywalking/oap-libs 目录下
 cp mysql-connector-java-8.0.17.jar skywalking8.6.0/oap-libs
@@ -53,7 +82,7 @@ cd skywalking8.6.0/bin
 访问http://ip:18080进入SkyWalking UI
 
 # 9、探针配置（Agent）
-vim /opt/skywalking/agent/config/agent.config
+vim skywalking8.6.0/agent/config/agent.config
 修改项目名字、日志打印级别、skywalking的服务地址
 
 
@@ -63,13 +92,16 @@ vim /opt/skywalking/agent/config/agent.config
 CATALINA_OPTS="$CATALINA_OPTS -javaagent:/opt/skywalking/agent/skywalking-agent.jar"
 export CATALINA_OPTS
 
-
 2、基于JAR file的服务(SpringBoot)
 在启动应用程序的命令行中添加 -javaagent 参数，并确保在-jar参数之前添加它，例如:
-java -javaagent:/opt/skywalking/agent/skywalking-agent.jar
+java -javaagent:/opt/taotao-cloud/skywalking8.6.0/agent/skywalking-agent.jar
 -Dskywalking.agent.service_name=taotao-cloud-gateway
+-Dskywalking.agent.authentication=taotao-cloud
+-Dskywalking.logging.file_name=taotao-cloud-gateway.skywalking.log
+-Dskywalking.logging.level=INFO
+-Dskywalking.logging.dir=../logs/application/taotao-cloud-gateway
 -Dskywalking.collector.backend_service=127.0.0.1:11800
--jar yourApp.jar
+-jar taotao-cloud-gateway.jar
 
 ##################### skywalking.sh #############################
 #!/bin/bash
--- config/cloud/zipkin2_23_2/install.sh
+++ config/cloud/zipkin2_23_2/install.sh
@@ -12,19 +12,19 @@ wget https://repo1.maven.org/maven2/io/zipkin/zipkin-server/2.23.2/zipkin-server
 # zipkin mysql脚本
 https://github.com/openzipkin/zipkin/blob/master/zipkin-storage/mysql-v1/src/main/resources/mysql.sql
 
-# java -jar zipkin-server-2.12.9-exec.jar
+# java -jar zipkin-server-2.23.2-exec.jar
 # --zipkin.collector.rabbitmq.addresses=localhost
 # --zipkin.collector.rabbitmq.username=xxx
 # --zipkin.collector.rabbitmq.password=xxx
 # --STORAGE_TYPE=mysql
-# --MYSQL_HOST=127.0.0.1
+# --MYSQL_HOST=192.168.1.10
 # --MYSQL_TCP_PORT=3306
-# --MYSQL_DB=zipkin
+# --MYSQL_DB=taotao-cloud-zipkin
 # --MYSQL_USER=root
-# --MYSQL_PASS=root
+# --MYSQL_PASS=123456
 
 # zipkin elasticsearch
-java -jar zipkin-server-2.12.9-exec.jar
+java -jar zipkin-server-2.23.2-exec.jar
 #--KAFKA_BOOTSTRAP_SERVERS=localhost:9092
 --STORAGE_TYPE=elasticsearch
 --ES_HOSTS=http://127.0.0.1:9200
@@ -33,7 +33,13 @@ java -jar zipkin-server-2.12.9-exec.jar
 #!/bin/bash
 
 function start_zipkin() {
-     nohup java -jar /opt/taotao-cloud/zipkin2.23.2/zipkin-server-2.23.2-exec.jar >/opt/taotao-cloud/zipkin2.23.2/start.log 2>&1 &
+     nohup java -jar /opt/taotao-cloud/zipkin2.23.2/zipkin-server-2.23.2-exec.jar \
+        --MYSQL_HOST=192.168.1.10 \
+        --MYSQL_TCP_PORT=3306 \
+        --MYSQL_DB=taotao-cloud-zipkin \
+        --MYSQL_USER=root \
+        --MYSQL_PASS=123456 \
+     >/opt/taotao-cloud/zipkin2.23.2/start.log 2>&1 &
      sleep 10
      echo " zipkin started"
 }
