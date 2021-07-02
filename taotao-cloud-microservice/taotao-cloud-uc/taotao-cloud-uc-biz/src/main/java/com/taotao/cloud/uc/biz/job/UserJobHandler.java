2bae10ca632787a3229c0df4089c8136d54cdc58
shuigedeng <981376577@qq.com>
2021-07-02 13:40:06 +0800

    [taotao-cloud-project-1189] update xxl job

--- taotao-cloud-microservice/taotao-cloud-uc/taotao-cloud-uc-biz/src/main/java/com/taotao/cloud/uc/biz/job/UserJobHandler.java
+++ taotao-cloud-microservice/taotao-cloud-uc/taotao-cloud-uc-biz/src/main/java/com/taotao/cloud/uc/biz/job/UserJobHandler.java
@@ -1,37 +1,36 @@
-///**
-// * Project Name: my-projects
-// * Package Name: com.taotao.cloud.uc.biz.job
-// * Date: 2020/6/16 14:43
-// * Author: dengtao
-// */
-//package com.taotao.cloud.uc.biz.job;
-//
-//import com.xxl.job.core.biz.model.ReturnT;
-//import com.xxl.job.core.handler.annotation.XxlJob;
-//import com.xxl.job.core.log.XxlJobLogger;
-//import org.springframework.stereotype.Component;
-//
-//import java.util.concurrent.TimeUnit;
-//
-///**
-// * <br>
-// *
-// * @author dengtao
-// * @version v1.0.0
-// * @since 2020/6/16 14:43
-// */
-//@Component
-//public class UserJobHandler {
-//
-//    @XxlJob("UserJobHandler")
-//    public ReturnT<String> userJobHandler(String param) throws Exception {
-//        XxlJobLogger.log("XXL-JOB, Hello World.");
-//
-//        for (int i = 0; i < 5; i++) {
-//            XxlJobLogger.log("beat at:" + i);
-//            System.out.println("XXL-JOB测试-----" + i);
-//            TimeUnit.SECONDS.sleep(2);
-//        }
-//        return ReturnT.SUCCESS;
-//    }
-//}
+/**
+ * Project Name: my-projects
+ * Package Name: com.taotao.cloud.uc.biz.job
+ * Date: 2020/6/16 14:43
+ * Author: dengtao
+ */
+package com.taotao.cloud.uc.biz.job;
+
+import com.xxl.job.core.biz.model.ReturnT;
+import com.xxl.job.core.context.XxlJobHelper;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import java.util.concurrent.TimeUnit;
+import org.springframework.stereotype.Component;
+
+/**
+ * <br>
+ *
+ * @author dengtao
+ * @version v1.0.0
+ * @since 2020/6/16 14:43
+ */
+@Component
+public class UserJobHandler {
+
+    @XxlJob("UserJobHandler")
+    public ReturnT<String> userJobHandler(String param) throws Exception {
+	    XxlJobHelper.log("XXL-JOB, Hello World.");
+
+        for (int i = 0; i < 5; i++) {
+	        XxlJobHelper.log("beat at:" + i);
+            System.out.println("XXL-JOB测试-----" + i);
+            TimeUnit.SECONDS.sleep(2);
+        }
+        return ReturnT.SUCCESS;
+    }
+}
--- taotao-cloud-microservice/taotao-cloud-visual/taotao-cloud-sentinel-dashboard/src/main/java/com/taotao/cloud/sentinel/dashboard/TaoTaoCloudSentinelDashboardApplication.java
+++ taotao-cloud-microservice/taotao-cloud-visual/taotao-cloud-sentinel-dashboard/src/main/java/com/taotao/cloud/sentinel/dashboard/TaoTaoCloudSentinelDashboardApplication.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2002-2021 the original author or authors.
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      https://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,23 +15,26 @@
  */
 package com.taotao.cloud.sentinel.dashboard;
 
+import com.alibaba.csp.sentinel.init.InitExecutor;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
- * TaoTaoCloudAdminServerApplication
+ * Sentinel dashboard application.
  *
- * @author dengtao
- * @version 1.0.0
- * @since 2020/11/26 下午7:55
+ * @author Carpenter Lee
  */
 @EnableDiscoveryClient
 @SpringBootApplication
 public class TaoTaoCloudSentinelDashboardApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TaoTaoCloudSentinelDashboardApplication.class, args);
-	}
+    public static void main(String[] args) {
+        triggerSentinelInit();
+        SpringApplication.run(TaoTaoCloudSentinelDashboardApplication.class, args);
+    }
 
+    private static void triggerSentinelInit() {
+        new Thread(() -> InitExecutor.doInit()).start();
+    }
 }
