3c3f16bc7ab787fd3c1267c71f0089fd83853653
shuigedeng <981376577@qq.com>
2021-07-20 09:23:05 +0800

    [taotao-cloud-project-1226] custom loadbalancer

--- taotao-cloud-microservice/taotao-cloud-starter/taotao-cloud-starter-loadbalancer/src/main/java/com/taotao/cloud/loadbalancer/isolation/LbIsolationComponent.java
+++ taotao-cloud-microservice/taotao-cloud-starter/taotao-cloud-starter-loadbalancer/src/main/java/com/taotao/cloud/loadbalancer/isolation/LbIsolationComponent.java
@@ -1,32 +1,48 @@
-// /*
-//  * Copyright 2017-2020 original authors
-//  *
-//  * Licensed under the Apache License, Version 2.0 (the "License");
-//  * you may not use this file except in compliance with the License.
-//  * You may obtain a copy of the License at
-//  *
-//  * https://www.apache.org/licenses/LICENSE-2.0
-//  *
-//  * Unless required by applicable law or agreed to in writing, software
-//  * distributed under the License is distributed on an "AS IS" BASIS,
-//  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  * See the License for the specific language governing permissions and
-//  * limitations under the License.
-//  */
-// package com.taotao.cloud.ribbon.lb;
-//
-// import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-// import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
-//
-// /**
-//  * Ribbon扩展配置类
-//  *
-//  * @author shuigedeng
-//  * @since 2020/6/15 11:31
-//  * @version 1.0.0
-//  */
-// @ConditionalOnProperty(value = "taotao.cloud.ribbon.isolation.enabled", havingValue = "true")
-// @LoadBalancerClient(configuration = {RuleConfigure.class})
-// public class LbIsolationComponent {
-//
-// }
+ /*
+  * Copyright 2017-2020 original authors
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  * https://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+ package com.taotao.cloud.ribbon.lb;
+
+ import com.taotao.cloud.loadbalancer.isolation.CustomLoadBalancer;
+ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+ import org.springframework.cloud.client.ServiceInstance;
+ import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+ import org.springframework.cloud.loadbalancer.core.ReactorLoadBalancer;
+ import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+ import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
+ import org.springframework.context.annotation.Bean;
+ import org.springframework.core.env.Environment;
+
+ /**
+  * Ribbon扩展配置类
+  *
+  * @author shuigedeng
+  * @version 1.0.0
+  * @since 2020/6/15 11:31
+  */
+ @ConditionalOnProperty(value = "taotao.cloud.ribbon.isolation.enabled", havingValue = "true")
+ @LoadBalancerClient
+ public class LbIsolationComponent {
+
+	 @Bean
+	 public ReactorLoadBalancer<ServiceInstance> reactorServiceInstanceLoadBalancer(
+		 Environment environment,
+		 LoadBalancerClientFactory loadBalancerClientFactory) {
+		 String name = environment.getProperty(LoadBalancerClientFactory.PROPERTY_NAME);
+		 return new CustomLoadBalancer(
+			 loadBalancerClientFactory.getLazyProvider(name, ServiceInstanceListSupplier.class));
+	 }
+
+ }
