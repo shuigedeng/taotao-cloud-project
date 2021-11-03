43a26b8651355c51d7662337e0f5afe269627e46
shuigedeng <981376577@qq.com>
2021-11-03 11:20:04 +0800

    [taotao-cloud-project-1300] fix[taotao-cloud-order-biz build docker]

--- taotao-cloud-microservice/taotao-cloud-uc/taotao-cloud-uc-biz/src/main/java/com/taotao/cloud/uc/biz/TaoTaoCloudUcApplication.java
+++ taotao-cloud-microservice/taotao-cloud-uc/taotao-cloud-uc-biz/src/main/java/com/taotao/cloud/uc/biz/TaoTaoCloudUcApplication.java
@@ -25,7 +25,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 /**
  * TaoTaoCloudUcApplication
  * 抑制java9 module 报错
- * --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.lang.reflect=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED --add-opens jdk.management/com.sun.management.internal=ALL-UNNAMED
+ * --add-opens java.base/java.lang=ALL-UNNAMED
+ * --add-opens java.base/java.lang.reflect=ALL-UNNAMED
+ * --add-opens java.base/java.util=ALL-UNNAMED
+ * --add-opens jdk.management/com.sun.management.internal=ALL-UNNAMED
+ * --add-opens java.base/java.math=ALL-UNNAMED
  *
  * @author shuigedeng
  * @version 1.0.0
