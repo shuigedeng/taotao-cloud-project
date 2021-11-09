8ce3c886bcd9b33d98fede5a1a094cb4c7de3140
shuigedeng <981376577@qq.com>
2021-11-09 16:16:08 +0800

    [taotao-cloud-project-1309] add[spring cloud stream kafka]

---
+++ taotao-cloud-microservice/taotao-cloud-order/taotao-cloud-order-biz/src/main/java/com/taotao/cloud/order/biz/pulsar/ProductConsumer.java
@@ -0,0 +1,23 @@
+package com.taotao.cloud.order.biz.pulsar;
+
+import com.taotao.cloud.common.utils.LogUtil;
+import io.github.majusko.pulsar.PulsarMessage;
+import io.github.majusko.pulsar.annotation.PulsarConsumer;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductConsumer {
+    
+    @PulsarConsumer(topic=Product.PRODUCT_TOPIC, clazz= Product.class)
+    void consume(Product product) {
+    	// TODO process your message
+    	LogUtil.info(product.getData());
+    }
+
+
+	@PulsarConsumer(topic=Product.PRODUCT_TOPIC, clazz=Product.class)
+	void consume(PulsarMessage<Product> message) {
+		LogUtil.info(message.getValue().toString());
+		//producer.send(TOPIC, msg.getValue());
+	}
+}
---
+++ taotao-cloud-microservice/taotao-cloud-starter/taotao-cloud-starter-processor/src/main/java/com/taotao/cloud/processor/Version.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2002-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.taotao.cloud.processor;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Version
+ *
+ * @author shuigedeng
+ * @version v1.0
+ * @since 2021/03/11 14:12
+ */
+//@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.SOURCE)
+@Inherited
+@Documented
+public @interface Version {
+	int major () default 1; //主版本号
+	int minor () default 0; //子版本号
+}
+
