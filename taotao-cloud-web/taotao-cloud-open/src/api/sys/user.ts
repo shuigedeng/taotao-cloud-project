46a2a4913e2b7a764f49011193f27e687e61d00d
shuigedeng <981376577@qq.com>
2021-12-29 17:29:47 +0800

    [taotao-cloud-project-13890] update[taotao-cloud-oauth2-biz]

--- taotao-cloud-web/taotao-cloud-open/src/api/sys/user.ts
+++ taotao-cloud-web/taotao-cloud-open/src/api/sys/user.ts
@@ -14,7 +14,7 @@ enum Api {
   Logout = '/mate-uaa/auth/logout',
   GetUserInfo = '/mate-uaa/auth/get/user',
   GetPermCode = '/getPermCode',
-  GetCaptcha = '/oauth2/qrcode/code',
+  GetCaptcha = '/oauth2/captcha/code',
 }
 
 /**
