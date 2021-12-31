3f25387e2526eb6c13c2adf2d0bae14fa5130e0e
shuigedeng <981376577@qq.com>
2021-12-31 09:35:42 +0800

    [taotao-cloud-project-1394] update[taotao-cloud-admin taotao-cloud-gateway  taotao-cloud-auth taotao-cloud-xxljob  taotao-cloud-sys]

--- taotao-cloud-microservice/taotao-cloud-xxljob/src/main/java/com/taotao/cloud/xxljob/TaoTaoCloudXxlJobApplication.java
+++ taotao-cloud-microservice/taotao-cloud-xxljob/src/main/java/com/taotao/cloud/xxljob/TaoTaoCloudXxlJobApplication.java
@@ -31,6 +31,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class TaoTaoCloudXxlJobApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TaoTaoCloudXxlJobApplication.class, args);
 	}
 
