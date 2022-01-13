5d1696dd440990e01b6ad9d7a78b2d8ec199ba39
shuigedeng <981376577@qq.com>
2022-01-13 16:33:24 +0800

    [taotao-cloud-project-1350] update[ gateway sign and taotao-cloud-web sign  ]

--- taotao-cloud-microservice/taotao-cloud-sys/taotao-cloud-sys-biz/src/main/java/com/taotao/cloud/sys/biz/springboot/mysql/jpa/ImookCourseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.taotao.cloud.sys.biz.springboot.mysql.jpa;
-
-import com.hrhx.springboot.domain.ImookCourse;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImookCourseRepository extends JpaRepository<ImookCourse, Long> {
-}
--- taotao-cloud-microservice/taotao-cloud-sys/taotao-cloud-sys-biz/src/main/java/com/taotao/cloud/sys/biz/springboot/mysql/service/ImookCourseService.java
+++
@@ -1,4 +0,0 @@
-package com.taotao.cloud.sys.biz.springboot.mysql.service;
-
-public interface ImookCourseService {
-}
