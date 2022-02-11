393b4a49187cb404f3316db9e4ff5f729ba3ba75
shuigedeng <981376577@qq.com>
2022-02-11 09:05:26 +0800

    [taotao-cloud-project-1358] update[ spring-cloud-stream rocketmq ]

--- taotao-cloud-microservice/taotao-cloud-sys/taotao-cloud-sys-biz/src/main/java/com/taotao/cloud/sys/biz/TaoTaoCloudSysApplication.java
+++ taotao-cloud-microservice/taotao-cloud-sys/taotao-cloud-sys-biz/src/main/java/com/taotao/cloud/sys/biz/TaoTaoCloudSysApplication.java
@@ -18,6 +18,7 @@ package com.taotao.cloud.sys.biz;
 import com.taotao.cloud.web.annotation.TaoTaoCloudApplication;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
@@ -37,8 +38,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = "com.taotao.cloud.sys.biz.repository.inf")
 @TaoTaoCloudApplication
 public class TaoTaoCloudSysApplication {
-
+	private StreamBridge streamBridge;
 	public static void main(String[] args) {
+		streamBridge.send()
 		SpringApplication.run(TaoTaoCloudSysApplication.class, args);
 	}
 
