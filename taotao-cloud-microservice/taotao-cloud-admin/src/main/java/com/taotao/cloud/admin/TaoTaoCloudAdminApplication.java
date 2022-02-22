b0b4b8056746231f8ec9fc26e33105769ad518bb
shuigedeng <981376577@qq.com>
2022-02-22 15:14:04 +0800

    [taotao-cloud-project-1379] taotao-cloud-docker-image[taotao-cloud-sys taotao-cloud-admin ]

--- taotao-cloud-microservice/taotao-cloud-admin/src/main/java/com/taotao/cloud/admin/TaoTaoCloudAdminApplication.java
+++ taotao-cloud-microservice/taotao-cloud-admin/src/main/java/com/taotao/cloud/admin/TaoTaoCloudAdminApplication.java
@@ -33,6 +33,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class TaoTaoCloudAdminApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TaoTaoCloudAdminApplication.class, args);
 	}
 
