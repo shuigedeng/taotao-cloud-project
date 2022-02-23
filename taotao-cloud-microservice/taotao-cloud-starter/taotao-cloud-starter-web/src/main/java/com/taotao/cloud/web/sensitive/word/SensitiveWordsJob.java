d36409e113092f581eefb6da55316742d1a1782a
shuigedeng <981376577@qq.com>
2022-02-23 09:43:36 +0800

    [taotao-cloud-project-1381] update[ sensitive quartz ]

--- taotao-cloud-microservice/taotao-cloud-starter/taotao-cloud-starter-web/src/main/java/com/taotao/cloud/web/sensitive/word/SensitiveWordsJob.java
+++ taotao-cloud-microservice/taotao-cloud-starter/taotao-cloud-starter-web/src/main/java/com/taotao/cloud/web/sensitive/word/SensitiveWordsJob.java
@@ -7,39 +7,50 @@ import com.taotao.cloud.common.utils.LogUtil;
 import com.taotao.cloud.redis.repository.RedisRepository;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 import org.quartz.JobExecutionContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
 /**
- * 间隔更新敏感词
+ * 定时更新敏感词信息
  */
 public class SensitiveWordsJob extends QuartzJobBean {
 
 	@Autowired
 	private RedisRepository redisRepository;
-	private int code = 0;
+	private volatile int code = 0;
 
-	/**
-	 * 定时更新敏感词信息
-	 */
 	@Override
 	protected void executeInternal(JobExecutionContext jobExecutionContext) {
-		EXECUTOR.execute(() -> {
+		Future<Integer> submit = EXECUTOR.submit(() -> {
 			Object words = redisRepository.get(RedisConstant.SENSITIVE_WORDS_KEY);
 			if (Objects.nonNull(words)) {
 				List<String> sensitives = (List<String>) words;
 				if (sensitives.isEmpty()) {
-					return;
+					return -1;
 				}
 				int code = words.hashCode();
 				LogUtil.info("敏感词更新，code={}", code);
 				LogUtil.info("敏感词更新，this.code={}", this.code);
 				if (this.code != code) {
 					SensitiveWordsFilter.init(sensitives);
-					this.code = code;
+					return code;
 				}
 			}
+
+			return -1;
 		});
+
+		try {
+			Integer integer = submit.get();
+			if (integer != -1) {
+				this.code = integer;
+			}
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
