19e76247a0486b8d80071ffc8c3d7bcb00940821
shuigedeng <981376577@qq.com>
2022-03-04 14:38:42 +0800

    [taotao-cloud-project-1399] update[ sys es]

--- taotao-cloud-microservice/taotao-cloud-sys/taotao-cloud-sys-biz/src/main/java/com/taotao/cloud/sys/biz/forest/ClusterApis.java
+++ taotao-cloud-microservice/taotao-cloud-sys/taotao-cloud-sys-biz/src/main/java/com/taotao/cloud/sys/biz/forest/ClusterApis.java
@@ -2,7 +2,6 @@ package com.taotao.cloud.sys.biz.forest;
 
 import com.alibaba.fastjson.JSONObject;
 import com.dtflys.forest.annotation.*;
-
 public interface ClusterApis {
 
     @Request(
@@ -11,42 +10,42 @@ public interface ClusterApis {
     )
     JSONObject clusterState(@Var("baseUrl") String baseUrl);
 
-    @Request(
-            url = "${baseUrl}/_stats?format=json",
-            dataType = "json"
-    )
-    JSONObject status(@Var("baseUrl")  String baseUrl);
-
-    @Request(
-            url = "${baseUrl}/_nodes?format=json",
-            dataType = "json"
-    )
-    JSONObject clusterNodes(@Var("baseUrl")  String baseUrl);
-
-    @Request(
-            url = "${baseUrl}/_nodes/stats?format=json",
-            dataType = "json"
-    )
-    JSONObject nodeStats(@Var("baseUrl")  String baseUrl);
-
-
-    @Request(
-            url = "${baseUrl}/_cluster/health?format=json",
-            dataType = "json"
-    )
-    JSONObject clusterHealth(@Var("baseUrl")  String baseUrl);
-
-    @Post(
-            url = "${baseUrl}/${indexName}/_search?format=json",
-            dataType = "json",
-            contentType = "application/json"
-    )
-    JSONObject indexDataSearch(@Var("baseUrl") String baseUrl, @Var("indexName") String indexName, @Body String dsl);
-
-    @Post(
-            url = "${baseUrl}/_search?format=json",
-            dataType = "json",
-            contentType = "application/json"
-    )
-    JSONObject dslSearch(@Var("baseUrl") String baseUrl,@Body String dsl);
+    //@Request(
+    //        url = "${baseUrl}/_stats?format=json",
+    //        dataType = "json"
+    //)
+    //JSONObject status(@Var("baseUrl")  String baseUrl);
+	//
+    //@Request(
+    //        url = "${baseUrl}/_nodes?format=json",
+    //        dataType = "json"
+    //)
+    //JSONObject clusterNodes(@Var("baseUrl")  String baseUrl);
+	//
+    //@Request(
+    //        url = "${baseUrl}/_nodes/stats?format=json",
+    //        dataType = "json"
+    //)
+    //JSONObject nodeStats(@Var("baseUrl")  String baseUrl);
+	//
+	//
+    //@Request(
+    //        url = "${baseUrl}/_cluster/health?format=json",
+    //        dataType = "json"
+    //)
+    //JSONObject clusterHealth(@Var("baseUrl")  String baseUrl);
+	//
+    //@Post(
+    //        url = "${baseUrl}/${indexName}/_search?format=json",
+    //        dataType = "json",
+    //        contentType = "application/json"
+    //)
+    //JSONObject indexDataSearch(@Var("baseUrl") String baseUrl, @Var("indexName") String indexName, @Body String dsl);
+	//
+    //@Post(
+    //        url = "${baseUrl}/_search?format=json",
+    //        dataType = "json",
+    //        contentType = "application/json"
+    //)
+    //JSONObject dslSearch(@Var("baseUrl") String baseUrl,@Body String dsl);
 }
