005b45748f7c000c9bca127fa6592271b3bd7274
shuigedeng <981376577@qq.com>
2022-03-04 09:19:58 +0800

    [taotao-cloud-project-1398] update[ sys es]

--- taotao-cloud-microservice/taotao-cloud-sys/taotao-cloud-sys-biz/src/main/java/com/taotao/cloud/sys/biz/service/impl/IndexServiceImpl.java
+++ taotao-cloud-microservice/taotao-cloud-sys/taotao-cloud-sys-biz/src/main/java/com/taotao/cloud/sys/biz/service/impl/IndexServiceImpl.java
@@ -1,31 +1,6 @@
 package com.taotao.cloud.sys.biz.service.impl;
 
-import com.alibaba.fastjson.JSON;
 import com.taotao.cloud.sys.biz.service.IndexService;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import org.elasticsearch.ElasticsearchException;
-import org.elasticsearch.action.DocWriteRequest;
-import org.elasticsearch.action.DocWriteResponse;
-import org.elasticsearch.action.bulk.BulkItemResponse;
-import org.elasticsearch.action.bulk.BulkRequest;
-import org.elasticsearch.action.bulk.BulkResponse;
-import org.elasticsearch.action.delete.DeleteRequest;
-import org.elasticsearch.action.delete.DeleteResponse;
-import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.action.index.IndexResponse;
-import org.elasticsearch.action.update.UpdateRequest;
-import org.elasticsearch.action.update.UpdateResponse;
-import org.elasticsearch.client.RequestOptions;
-import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.client.indices.CreateIndexRequest;
-import org.elasticsearch.client.indices.GetIndexRequest;
-import org.elasticsearch.common.xcontent.XContentBuilder;
-import org.elasticsearch.common.xcontent.XContentType;
-import org.elasticsearch.rest.RestStatus;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
@@ -183,37 +158,37 @@ public class IndexServiceImpl implements IndexService {
 	//@Override
 	//public void indexDocsWithRouting(String indexName, List<Map<String, Object>> docs) {
 	//	try {
-    //        if (null == docs || docs.size() <= 0) {
-    //            return;
-    //        }
-    //        BulkRequest request = new BulkRequest();
-    //        for (Map<String, Object> doc : docs) {
-    //        	HashMap<String, Object> join = (HashMap<String, Object>)doc.get("joinkey");
-    //        	String route = (String)join.get("parent");
-    //            request.add(new IndexRequest(indexName).id((String)doc.get("key"))
-    //                        .source(doc).routing(route));
-    //        }
-    //        BulkResponse bulkResponse = client.bulk(request, RequestOptions.DEFAULT);
-    //        if (bulkResponse != null) {
-    //            for (BulkItemResponse bulkItemResponse : bulkResponse) {
-    //                DocWriteResponse itemResponse = bulkItemResponse.getResponse();
+	//        if (null == docs || docs.size() <= 0) {
+	//            return;
+	//        }
+	//        BulkRequest request = new BulkRequest();
+	//        for (Map<String, Object> doc : docs) {
+	//        	HashMap<String, Object> join = (HashMap<String, Object>)doc.get("joinkey");
+	//        	String route = (String)join.get("parent");
+	//            request.add(new IndexRequest(indexName).id((String)doc.get("key"))
+	//                        .source(doc).routing(route));
+	//        }
+	//        BulkResponse bulkResponse = client.bulk(request, RequestOptions.DEFAULT);
+	//        if (bulkResponse != null) {
+	//            for (BulkItemResponse bulkItemResponse : bulkResponse) {
+	//                DocWriteResponse itemResponse = bulkItemResponse.getResponse();
 	//
-    //                if (bulkItemResponse.getOpType() == DocWriteRequest.OpType.INDEX
-    //                        || bulkItemResponse.getOpType() == DocWriteRequest.OpType.CREATE) {
-    //                    IndexResponse indexResponse = (IndexResponse) itemResponse;
-    //                    System.out.println("新增成功" + indexResponse.toString());
-    //                } else if (bulkItemResponse.getOpType() == DocWriteRequest.OpType.UPDATE) {
-    //                    UpdateResponse updateResponse = (UpdateResponse) itemResponse;
-    //                    System.out.println("修改成功" + updateResponse.toString());
-    //                } else if (bulkItemResponse.getOpType() == DocWriteRequest.OpType.DELETE) {
-    //                    DeleteResponse deleteResponse = (DeleteResponse) itemResponse;
-    //                    System.out.println("删除成功" + deleteResponse.toString());
-    //                }
-    //            }
-    //        }
-    //    } catch (IOException e) {
-    //        e.printStackTrace();
-    //    }
+	//                if (bulkItemResponse.getOpType() == DocWriteRequest.OpType.INDEX
+	//                        || bulkItemResponse.getOpType() == DocWriteRequest.OpType.CREATE) {
+	//                    IndexResponse indexResponse = (IndexResponse) itemResponse;
+	//                    System.out.println("新增成功" + indexResponse.toString());
+	//                } else if (bulkItemResponse.getOpType() == DocWriteRequest.OpType.UPDATE) {
+	//                    UpdateResponse updateResponse = (UpdateResponse) itemResponse;
+	//                    System.out.println("修改成功" + updateResponse.toString());
+	//                } else if (bulkItemResponse.getOpType() == DocWriteRequest.OpType.DELETE) {
+	//                    DeleteResponse deleteResponse = (DeleteResponse) itemResponse;
+	//                    System.out.println("删除成功" + deleteResponse.toString());
+	//                }
+	//            }
+	//        }
+	//    } catch (IOException e) {
+	//        e.printStackTrace();
+	//    }
 	//}
 	//
 	//
