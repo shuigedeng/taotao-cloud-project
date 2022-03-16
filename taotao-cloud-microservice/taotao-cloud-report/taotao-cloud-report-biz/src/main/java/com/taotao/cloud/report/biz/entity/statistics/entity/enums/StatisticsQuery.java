18f94fdd3b457c40dba84fd540124059508afb93
shuigedeng <981376577@qq.com>
2022-03-16 16:16:24 +0800

    [taotao-cloud-project-1422] add[ configurations ]

---
+++ taotao-cloud-microservice/taotao-cloud-report/taotao-cloud-report-biz/src/main/java/com/taotao/cloud/report/biz/entity/statistics/entity/enums/StatisticsQuery.java
@@ -0,0 +1,19 @@
+package com.taotao.cloud.report.biz.entity.statistics.entity.enums;
+
+/**
+ * 统计查询
+ * @author Chopper
+ * @since 2021/2/9 16:17
+ */
+public enum StatisticsQuery {
+
+    /**
+     * 数量
+     */
+    NUM,
+
+    /**
+     * 金额
+     */
+    PRICE,
+}
---
+++ taotao-cloud-microservice/taotao-cloud-sys/taotao-cloud-sys-biz/src/main/java/com/taotao/cloud/sys/biz/entity/system/mapper/VerificationSourceMapper.java
@@ -0,0 +1,14 @@
+package com.taotao.cloud.sys.biz.entity.system.mapper;
+
+import cn.lili.modules.verification.entity.dos.VerificationSource;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 验证码资源维护数据处理层
+ *
+ * @author Chopper
+ * @since 2021/1/30 4:17 下午
+ */
+public interface VerificationSourceMapper extends BaseMapper<VerificationSource> {
+
+}
