55bab7b16f7be45da3d5d1f8749937b5417216f6
shuigedeng <981376577@qq.com>
2022-04-20 08:39:40 +0800

    [taotao-cloud-project-1477] update[ sys test ]

--- taotao-cloud-microservice/taotao-cloud-sys/taotao-cloud-sys-biz/src/test/java/com/taotao/cloud/sys/biz/TaoTaoCloudSysApplicationTests.java
+++ taotao-cloud-microservice/taotao-cloud-sys/taotao-cloud-sys-biz/src/test/java/com/taotao/cloud/sys/biz/TaoTaoCloudSysApplicationTests.java
@@ -1,14 +1,25 @@
 package com.taotao.cloud.sys.biz;
 
+import com.taotao.cloud.sys.biz.service.IFileService;
 import org.junit.jupiter.api.Test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TaoTaoCloudSysApplicationTests {
 
+	@Autowired
+	private IFileService fileService;
+
 	@Test
 	void contextLoads() {
+		fileService.findFileById(1L);
+	}
+
+	@Test
+	public void test1(){
+		fileService.findFileById(1L);
 	}
 
 }
