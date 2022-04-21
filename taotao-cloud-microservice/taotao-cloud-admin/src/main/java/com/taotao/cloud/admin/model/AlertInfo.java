8917ffbb3c257986f2c53ed3ee2b7b68a7c5abcf
shuigedeng <981376577@qq.com>
2022-04-21 14:05:19 +0800

    [taotao-cloud-project-1494] update[ license copyright]

--- taotao-cloud-microservice/taotao-cloud-admin/src/main/java/com/taotao/cloud/admin/model/AlertInfo.java
+++ taotao-cloud-microservice/taotao-cloud-admin/src/main/java/com/taotao/cloud/admin/model/AlertInfo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2021 the original author or authors.
+ * Copyright 2020-2030, Shuigedeng (981376577@qq.com & https://blog.taotaocloud.top/).
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.taotao.cloud.admin.model;
 
 import java.io.Serializable;
