8ac09f6d7cf45b58f23a3ea4898f6b7ab038e38e
shuigedeng <981376577@qq.com>
2022-04-27 15:32:49 +0800

    [taotao-cloud-project-1503] update[ recommend ]

--- taotao-cloud-microservice/taotao-cloud-report/taotao-cloud-report-biz/src/main/java/com/taotao/cloud/report/biz/mapper/MemberStatisticsMapper.java
+++ taotao-cloud-microservice/taotao-cloud-report/taotao-cloud-report-biz/src/main/java/com/taotao/cloud/report/biz/mapper/MemberStatisticsMapper.java
@@ -3,6 +3,7 @@ package com.taotao.cloud.report.biz.mapper;
 import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.toolkit.Constants;
+import com.taotao.cloud.report.biz.entity.MemberStatisticsData;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
--- taotao-cloud-microservice/taotao-cloud-report/taotao-cloud-report-biz/src/main/java/com/taotao/cloud/report/biz/mapper/OrderComplaintStatisticsMapper.java
+++
@@ -1,9 +0,0 @@
-package com.taotao.cloud.report.biz.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-/**
- * 订单投诉数据处理层
- */
-public interface OrderComplaintStatisticsMapper extends BaseMapper<OrderComplaint> {
-}
