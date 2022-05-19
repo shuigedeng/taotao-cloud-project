6562bb9eafd1bfc1c7032a0802d9143a7f13f700
shuigedeng <981376577@qq.com>
2022-05-19 09:27:57 +0800

    [taotao-cloud-project-1504] update [ code ]

---
+++ taotao-cloud-microservice/taotao-cloud-starter/taotao-cloud-starter-redis/src/main/java/com/taotao/cloud/redis/configuration/IdGeneratorAutoConfiguration.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2020-2030, Shuigedeng (981376577@qq.com & https://blog.taotaocloud.top/).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.taotao.cloud.redis.configuration;
+
+import com.taotao.cloud.common.constant.StarterName;
+import com.taotao.cloud.common.utils.log.LogUtil;
+import com.taotao.cloud.redis.repository.RedisRepository;
+import com.taotao.cloud.redis.runner.IdGeneratorCommandLineRunner;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * TaoTaoCloudRedisAutoConfiguration
+ *
+ * @author shuigedeng
+ * @version 2021.9
+ * @since 2021-09-07 21:17:02
+ */
+@Configuration
+@ConditionalOnBean({RedisRepository.class})
+@Import(IdGeneratorCommandLineRunner.class)
+public class IdGeneratorAutoConfiguration implements InitializingBean {
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		LogUtil.started(IdGeneratorAutoConfiguration.class, StarterName.REDIS_STARTER);
+	}
+}
