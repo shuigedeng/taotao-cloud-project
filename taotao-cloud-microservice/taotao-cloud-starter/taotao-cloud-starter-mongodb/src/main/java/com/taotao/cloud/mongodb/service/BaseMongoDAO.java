b50deff457bf0056e0d662283189c7e69b753663
shuigedeng <981376577@qq.com>
2022-06-06 09:23:31 +0800

    [taotao-cloud-project-1561] update[ gateway ]

--- taotao-cloud-microservice/taotao-cloud-starter/taotao-cloud-starter-mongodb/src/main/java/com/taotao/cloud/mongodb/service/BaseMongoDAO.java
+++ taotao-cloud-microservice/taotao-cloud-starter/taotao-cloud-starter-mongodb/src/main/java/com/taotao/cloud/mongodb/service/BaseMongoDAO.java
@@ -1,41 +1,40 @@
 package com.taotao.cloud.mongodb.service;
 
-import com.taotao.cloud.common.model.PageModel;
-import java.util.List;
-import java.util.Set;
+import com.taotao.cloud.mongodb.helper.bean.Page;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 
+import java.util.List;
+
 /**
- * 基地mongo刀
+ * MongoDB通用Dao
  *
- * @author shuigedeng
- * @version 2022.05
- * @since 2022-05-27 21:47:01
+ * @param <T>
  */
-public interface BaseMongoDAO {
+public interface BaseMongoDAO<T> {
 
 	/**
 	 * 保存一个对象到mongodb
 	 *
 	 * @param entity
+	 * @return
 	 */
-	<T> T save(T entity);
+	public T save(T entity);
 
 	/**
 	 * 根据id删除对象
 	 *
 	 * @param t
 	 */
-	<T> void deleteById(T t);
+	public void deleteById(T t);
 
 	/**
 	 * 根据对象的属性删除
 	 *
 	 * @param t
 	 */
-	<T> void deleteByCondition(T t);
+	public void deleteByCondition(T t);
 
 
 	/**
@@ -44,17 +43,16 @@ public interface BaseMongoDAO {
 	 * @param id
 	 * @param t
 	 */
-	<T> void updateById(String id, T t);
+	public void updateById(String id, T t);
 
 
 	/**
 	 * 根据对象的属性查询
 	 *
 	 * @param t
-	 * @return {@link List }<{@link T }>
-	 * @since 2022-05-27 21:46:03
+	 * @return
 	 */
-	<T> List<T> findByCondition(T t);
+	public List<T> findByCondition(T t);
 
 
 	/**
@@ -62,25 +60,24 @@ public interface BaseMongoDAO {
 	 *
 	 * @param query
 	 */
-	<T> List<T> find(Query query);
+	public List<T> find(Query query);
 
 	/**
 	 * 通过一定的条件查询一个实体
 	 *
 	 * @param query
-	 * @return {@link T }
-	 * @since 2022-05-27 21:45:57
+	 * @return
 	 */
-	<T> T findOne(Query query);
+	public T findOne(Query query);
 
 	/**
 	 * 通过条件查询更新数据
 	 *
 	 * @param query
 	 * @param update
-	 * @since 2022-05-27 21:45:55
+	 * @return
 	 */
-	void update(Query query, Update update);
+	public void update(Query query, Update update);
 
 	/**
 	 * 通过ID获取记录
@@ -88,44 +85,39 @@ public interface BaseMongoDAO {
 	 * @param id
 	 * @return
 	 */
-	<T> T findById(String id);
+	public T findById(String id);
 
 	/**
 	 * 通过ID获取记录,并且指定了集合名(表的意思)
 	 *
 	 * @param id
 	 * @param collectionName 集合名
+	 * @return
 	 */
-	<T> T findById(String id, String collectionName);
+	public T findById(String id, String collectionName);
 
 	/**
 	 * 通过条件查询,查询分页结果
 	 *
 	 * @param page
 	 * @param query
+	 * @return
 	 */
-	<T> PageModel<T> findPage(PageModel<T> page, Query query);
+	public Page<T> findPage(Page<T> page, Query query);
 
 	/**
 	 * 求数据总和
 	 *
 	 * @param query
+	 * @return
 	 */
-	long count(Query query);
+	public long count(Query query);
+
 
 	/**
 	 * 获取MongoDB模板操作
 	 *
-	 * @return {@link MongoTemplate }
-	 * @since 2022-05-27 21:45:49
+	 * @return
 	 */
-	MongoTemplate getMongoTemplate();
-
-	/**
-	 * 把收集名字
-	 *
-	 * @return {@link Set }<{@link String }>
-	 * @since 2022-05-27 21:44:38
-	 */
-	Set<String> getCollectionNames();
+	public MongoTemplate getMongoTemplate();
 }
