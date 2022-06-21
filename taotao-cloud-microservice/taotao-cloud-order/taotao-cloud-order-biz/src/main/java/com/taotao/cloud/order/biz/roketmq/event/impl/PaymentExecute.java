28d0e2b16402594aa238a0a8f7faa62746b597f5
shuigedeng <n3nbhbn3ymb>
2022-06-21 17:49:31 +0800

    [taotao-cloud-project-1504] update [ code ]

--- taotao-cloud-microservice/taotao-cloud-order/taotao-cloud-order-biz/src/main/java/com/taotao/cloud/order/biz/roketmq/event/impl/PaymentExecute.java
+++ taotao-cloud-microservice/taotao-cloud-order/taotao-cloud-order-biz/src/main/java/com/taotao/cloud/order/biz/roketmq/event/impl/PaymentExecute.java
@@ -4,6 +4,7 @@ import com.egzosn.pay.paypal.bean.order.Payment;
 import com.taotao.cloud.common.utils.common.IdGeneratorUtil;
 import com.taotao.cloud.common.utils.context.ContextUtil;
 import com.taotao.cloud.common.utils.log.LogUtil;
+import com.taotao.cloud.order.api.enums.order.OrderStatusEnum;
 import com.taotao.cloud.order.api.message.OrderMessage;
 import com.taotao.cloud.order.api.enums.order.PayStatusEnum;
 import com.taotao.cloud.order.biz.entity.order.Order;
@@ -33,49 +34,33 @@ public class PaymentExecute implements OrderStatusChangeEvent {
 
 	@Override
 	public void orderChange(OrderMessage orderMessage) {
-		switch (orderMessage.newStatus()) {
-			case CANCELLED:
-				Order order = orderService.getBySn(orderMessage.orderSn());
+		if (orderMessage.getNewStatus() == OrderStatusEnum.CANCELLED) {
+			Order order = orderService.getBySn(orderMessage.getOrderSn());
 
-				//如果未付款，则不去要退回相关代码执行
-				if (order.getPayStatus().equals(PayStatusEnum.UNPAID.name())) {
-					return;
-				}
-				PaymentMethodEnum paymentMethodEnum = PaymentMethodEnum.valueOf(
-					order.getPaymentMethod());
-				//进行退款操作
-				switch (paymentMethodEnum) {
-					case WALLET:
-					case ALIPAY:
-					case WECHAT:
-						//获取支付方式
-						Payment payment =
-							(Payment) ContextUtil.getBean(paymentMethodEnum.getPlugin(), true);
+			//如果未付款，则不去要退回相关代码执行
+			if (order.getPayStatus().equals(PayStatusEnum.UNPAID.name())) {
+				return;
+			}
+			PaymentMethodEnum paymentMethodEnum = PaymentMethodEnum.valueOf(order.getPaymentMethod());
 
-						RefundLog refundLog = RefundLog.builder()
-							.isRefund(false)
-							.totalAmount(order.getFlowPrice())
-							.payPrice(order.getFlowPrice())
-							.memberId(order.getMemberId())
-							.paymentName(order.getPaymentMethod())
-							.afterSaleNo("订单取消")
-							.orderSn(order.getSn())
-							.paymentReceivableNo(order.getReceivableNo())
-							.outOrderNo("AF" + IdGeneratorUtil.getIdStr())
-							.outOrderNo("AF" + IdGeneratorUtil.getIdStr())
-							.refundReason("订单取消")
-							.build();
-						payment.cancel(refundLog);
-						break;
-					case BANK_TRANSFER:
-						break;
-					default:
-						LogUtil.error("订单支付执行异常,订单编号：{}", orderMessage.orderSn());
-						break;
-				}
-				break;
-			default:
-				break;
+			//获取支付方式
+			Payment payment =
+				(Payment) SpringContextUtil.getBean(paymentMethodEnum.getPlugin());
+
+			RefundLog refundLog = RefundLog.builder()
+				.isRefund(false)
+				.totalAmount(order.getFlowPrice())
+				.payPrice(order.getFlowPrice())
+				.memberId(order.getMemberId())
+				.paymentName(order.getPaymentMethod())
+				.afterSaleNo("订单取消")
+				.orderSn(order.getSn())
+				.paymentReceivableNo(order.getReceivableNo())
+				.outOrderNo("AF" + SnowFlake.getIdStr())
+				.outOrderNo("AF" + SnowFlake.getIdStr())
+				.refundReason("订单取消")
+				.build();
+			payment.refund(refundLog);
 		}
 	}
 }
