019ff7ecc3e1ffcd0a3e1a4b4ff83ecc7c314a5a
shuigedeng <981376577@qq.com>
2022-06-30 09:11:35 +0800

    [taotao-cloud-project-1595] update[ feign dubbo ]

--- taotao-cloud-microservice/taotao-cloud-starter/taotao-cloud-starter-redis/src/main/java/com/taotao/cloud/redis/redisson/handle/impl/OrderTimeoutNotEvaluated.java
+++ taotao-cloud-microservice/taotao-cloud-starter/taotao-cloud-starter-redis/src/main/java/com/taotao/cloud/redis/redisson/handle/impl/OrderTimeoutNotEvaluated.java
@@ -7,18 +7,26 @@ import org.springframework.stereotype.Component;
 
 /**
  * 订单超时未评价处理类
+ *
+ * @author shuigedeng
+ * @version 2022.06
+ * @since 2022-06-30 09:09:02
  */
 @Component
-public class OrderTimeoutNotEvaluated implements RedisDelayQueueHandle<Map> {
+public class OrderTimeoutNotEvaluated implements RedisDelayQueueHandle {
+
 	@Override
-	public void execute(Map map) {
-		// TODO 订单超时未评价，系统默认好评处理业务...
-		Long now = System.currentTimeMillis();
-		Long timestamp = Long.valueOf(String.valueOf(map.get("timestamp")));
-		Long delayTime = now - timestamp;
-		Long random = Long.valueOf(String.valueOf(map.get("random")));
-		Long diffTime = delayTime - random * 1000;
-		LogUtil.info("(OrderTimeoutNotEvaluated) orderId：{}, 预计延迟时间：{} 秒，实际延迟时间：{} 毫秒，相差：{} 毫秒",
+	public void execute(Object obj) {
+		if (obj instanceof Map map) {
+			// TODO 订单超时未评价，系统默认好评处理业务...
+			Long now = System.currentTimeMillis();
+			Long timestamp = Long.valueOf(String.valueOf(map.get("timestamp")));
+			Long delayTime = now - timestamp;
+			Long random = Long.valueOf(String.valueOf(map.get("random")));
+			Long diffTime = delayTime - random * 1000;
+			LogUtil.info("(OrderTimeoutNotEvaluated) orderId：{}, 预计延迟时间：{} 秒，实际延迟时间：{} 毫秒，相差：{} 毫秒",
 				map.get("orderId"), random, delayTime, diffTime);
+		}
+
 	}
 }
