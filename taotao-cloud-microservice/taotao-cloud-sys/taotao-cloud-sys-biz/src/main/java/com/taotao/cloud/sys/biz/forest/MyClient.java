f50cbed8d06cb95d0d921aca1c117289ffd0fe48
shuigedeng <981376577@qq.com>
2022-07-07 08:32:53 +0800

    [taotao-cloud-project-16078] update[ forest]

--- taotao-cloud-microservice/taotao-cloud-sys/taotao-cloud-sys-biz/src/main/java/com/taotao/cloud/sys/biz/forest/MyClient.java
+++ taotao-cloud-microservice/taotao-cloud-sys/taotao-cloud-sys-biz/src/main/java/com/taotao/cloud/sys/biz/forest/MyClient.java
@@ -1,20 +1,20 @@
-//package com.taotao.cloud.sys.biz.forest;
-//
-//import com.dtflys.forest.annotation.DataVariable;
-//import com.dtflys.forest.annotation.Get;
-//import com.dtflys.forest.annotation.Request;
-//
-//public interface MyClient {
-//
-//	@Request("http://localhost:8080/hello")
-//	String helloForest();
-//
-//	/**
-//	 * 在请求接口上加上自定义的 @MyAuth 注解
-//	 * 注解的参数可以是字符串模板，通过方法调用的时候动态传入
-//	 * 也可以是写死的字符串
-//	 */
-//	@Get("/hello/user?username={username}")
-//	@MyAuth(username = "{username}", password = "bar")
-//	String send(@DataVariable("username") String username);
-//}
+package com.taotao.cloud.sys.biz.forest;
+
+import com.dtflys.forest.annotation.DataVariable;
+import com.dtflys.forest.annotation.Get;
+import com.dtflys.forest.annotation.Request;
+
+public interface MyClient {
+
+	@Request("http://localhost:8080/hello")
+	String helloForest();
+
+	/**
+	 * 在请求接口上加上自定义的 @MyAuth 注解
+	 * 注解的参数可以是字符串模板，通过方法调用的时候动态传入
+	 * 也可以是写死的字符串
+	 */
+	@Get("/hello/user?username={username}")
+	@MyAuth(username = "{username}", password = "bar")
+	String send(@DataVariable("username") String username);
+}
