508e9950318c5298fc2f62d71183214dff73265b
shuigedeng <n3nbhbn3ymb>
2022-07-11 09:26:10 +0800

    [taotao-cloud-project-1612] add [ websocket ]

---
+++ taotao-cloud-microservice/taotao-cloud-sys/taotao-cloud-sys-biz/src/main/java/com/taotao/cloud/sys/biz/websockt/original/WsServerEndpoint.java
@@ -0,0 +1,47 @@
+package com.taotao.cloud.sys.biz.websockt.original;
+
+import org.springframework.stereotype.Component;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@ServerEndpoint("/original/websocket")
+@Component
+public class WsServerEndpoint {
+
+    /**
+     * 连接成功
+     *
+     * @param session
+     */
+    @OnOpen
+    public void onOpen(Session session) {
+        System.out.println("连接成功");
+    }
+
+    /**
+     * 连接关闭
+     *
+     * @param session
+     */
+    @OnClose
+    public void onClose(Session session) {
+        System.out.println("连接关闭");
+    }
+
+    /**
+     * 接收到消息
+     *
+     * @param text
+     */
+    @OnMessage
+    public String onMsg(String text) throws IOException {
+        return "servet 发送：" + text;
+    }
+}
