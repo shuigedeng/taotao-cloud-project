202b37a39af7269e83b7a2710248e66b9122f834
shuigedeng <n3nbhbn3ymb>
2022-07-19 14:25:33 +0800

    [taotao-cloud-project-1630] build [ starters ]

---
+++ taotao-cloud-microservice/taotao-cloud-graphql/src/main/java/com/taotao/cloud/graphql/project/ProjectStatus.java
@@ -0,0 +1,19 @@
+package com.taotao.cloud.graphql.project;
+
+import java.util.Arrays;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+public enum ProjectStatus {
+
+	ACTIVE, COMMUNITY, INCUBATING, ATTIC;
+
+	@JsonCreator
+	public static ProjectStatus fromName(String name) {
+		return Arrays.stream(ProjectStatus.values())
+				.filter(type -> type.name().equals(name))
+				.findFirst()
+				.orElse(ProjectStatus.ACTIVE);
+	}
+
+}
