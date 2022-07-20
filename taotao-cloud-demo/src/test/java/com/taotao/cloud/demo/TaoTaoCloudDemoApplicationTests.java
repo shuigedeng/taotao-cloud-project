1f2ef3625de2ddcd4eb6decf63579d9dada409f7
shuigedeng <981376577@qq.com>
2022-07-20 10:38:57 +0800

    [taotao-cloud-project-1631] update[ admin to monitor]

--- taotao-cloud-demo/src/test/java/com/taotao/cloud/demo/TaoTaoCloudDemoApplicationTests.java
+++ taotao-cloud-demo/src/test/java/com/taotao/cloud/demo/TaoTaoCloudDemoApplicationTests.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class TaoTaoCloudDemoApplicationTests {
 
 	@Autowired
 	private StandardOssClient standardOssClient;
--- taotao-cloud-reactive/taotao-cloud-reactive-common/src/main/java/com/taotao/cloud/reactive/common/ReactiveCommonApplication.java
+++ taotao-cloud-reactive/taotao-cloud-reactive-common/src/main/java/com/taotao/cloud/reactive/common/ReactiveCommonApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CommonApplication {
+public class ReactiveCommonApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CommonApplication.class, args);
+		SpringApplication.run(ReactiveCommonApplication.class, args);
 	}
 
 }
