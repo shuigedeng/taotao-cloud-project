399420e66f6764134d9f4a856fd743b2abfb8005
shuigedeng <981376577@qq.com>
2022-09-01 17:56:22 +0800

    [taotao-cloud-project-1631] update[ admin to monitor]

---
+++ taotao-cloud-microservice/taotao-cloud-recommend/taotao-cloud-recommend-biz/src/test/java/com/taotao/cloud/report/biz/TaoTaoCloudReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taotao.cloud.report.biz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaoTaoCloudReportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
