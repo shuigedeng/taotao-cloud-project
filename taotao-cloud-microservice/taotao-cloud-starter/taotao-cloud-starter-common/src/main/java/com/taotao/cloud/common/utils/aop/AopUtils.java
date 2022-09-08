7631021a6c93c976788250f1aff34103e59de3c5
shuigedeng <981376577@qq.com>
2022-09-08 16:58:11 +0800

    [taotao-cloud-project-1716] update[ druid ]

--- taotao-cloud-microservice/taotao-cloud-starter/taotao-cloud-starter-common/src/main/java/com/taotao/cloud/common/utils/aop/AopUtils.java
+++ taotao-cloud-microservice/taotao-cloud-starter/taotao-cloud-starter-common/src/main/java/com/taotao/cloud/common/utils/aop/AopUtils.java
@@ -16,8 +16,11 @@
 package com.taotao.cloud.common.utils.aop;
 
 import com.taotao.cloud.common.utils.log.LogUtils;
+import java.io.UnsupportedEncodingException;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import org.aspectj.lang.JoinPoint;
 import org.springframework.aop.framework.AdvisedSupport;
 import org.springframework.aop.framework.AopProxy;
@@ -111,4 +114,35 @@ public class AopUtils extends org.springframework.aop.support.AopUtils {
 		}
 		return null;
 	}
+
+	///**
+	// * MD5加密字符串（32位大写）
+	// *
+	// * @param string 需要进行MD5加密的字符串
+	// * @return 加密后的字符串（大写）
+	// */
+	//public static String md5Encrypt32Upper(String string) {
+	//	byte[] hash;
+	//	try {
+	//		// 创建一个MD5算法对象，并获得MD5字节数组,16*8=128位
+	//		hash = MessageDigest.getInstance("MD5").digest(string.getBytes("UTF-8"));
+	//	} catch (NoSuchAlgorithmException e) {
+	//		throw new RuntimeException("Huh, MD5 should be supported?", e);
+	//	} catch (UnsupportedEncodingException e) {
+	//		throw new RuntimeException("Huh, UTF-8 should be supported?", e);
+	//	}
+	//
+	//	// 转换为十六进制字符串
+	//	StringBuilder hex = new StringBuilder(hash.length * 2);
+	//	for (byte b : hash) {
+	//		if ((b & 0xFF) < 0x10) {
+	//			hex.append("0");
+	//		}
+	//		hex.append(Integer.toHexString(b & 0xFF));
+	//	}
+	//	return hex.toString().toUpperCase();
+	//}
+	//public static void main(String[] args) {
+	//	System.out.println(md5Encrypt32Upper("HQ7r*eBv9["));
+	//}
 }  
