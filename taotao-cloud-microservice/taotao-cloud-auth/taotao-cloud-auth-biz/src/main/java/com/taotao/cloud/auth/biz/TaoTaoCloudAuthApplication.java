748240c6e253d11e0ca8604ee51a104d4acfdd16
shuigedeng <981376577@qq.com>
2022-09-16 17:21:28 +0800

    [taotao-cloud-project-1723] update[ gen ]

--- taotao-cloud-microservice/taotao-cloud-auth/taotao-cloud-auth-biz/src/main/java/com/taotao/cloud/auth/biz/TaoTaoCloudAuthApplication.java
+++ taotao-cloud-microservice/taotao-cloud-auth/taotao-cloud-auth-biz/src/main/java/com/taotao/cloud/auth/biz/TaoTaoCloudAuthApplication.java
@@ -23,44 +23,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.apache.dubbo.common.serialize.kryo.utils.KryoUtils;
+
 /**
- * TaoTaoCloudAuthBizApplication
- * 默认url
- * 作用
- *
- * /oauth/authorize
- * 授权端点
- * /oauth/token
- * 令牌端点
- * /oauth/confirm_access
- * 用户批准授权的端点
- * /oauth/error
- * 用于渲染授权服务器的错误
- * /oauth/check_token
- * 资源服务器解码access token
- * /oauth/check_token
- *
+ * TaoTaoCloudAuthBizApplication 默认url 作用
+ * <p>
+ * /oauth/authorize 授权端点 /oauth/token 令牌端点 /oauth/confirm_access 用户批准授权的端点 /oauth/error 用于渲染授权服务器的错误
+ * /oauth/check_token 资源服务器解码access token /oauth/check_token
+ * <p>
  * 当使用JWT的时候，暴露公钥的端点
- *
- * 抑制java9 module 报错
- * --add-opens java.base/java.lang=ALL-UNNAMED
- * --add-opens java.base/java.lang.reflect=ALL-UNNAMED
- * --add-opens java.base/java.lang.invoke=ALL-UNNAMED
- * --add-opens java.base/java.util=ALL-UNNAMED
- * --add-opens jdk.management/com.sun.management.internal=ALL-UNNAMED
- * --add-opens java.base/java.math=ALL-UNNAMED
- * --add-opens java.base/sun.reflect.annotation=ALL-UNNAMED
- * --add-exports java.desktop/sun.awt=ALL-UNNAMED
- * --add-exports java.desktop/sun.font=ALL-UNNAMED
- * --add-opens java.desktop/sun.awt=ALL-UNNAMED
- * --add-opens java.desktop/sun.font=ALL-UNNAMED
+ * <p>
+ * 抑制java9 module 报错 --add-opens java.base/java.lang=ALL-UNNAMED --add-opens
+ * java.base/java.lang.reflect=ALL-UNNAMED --add-opens java.base/java.lang.invoke=ALL-UNNAMED
+ * --add-opens java.base/java.util=ALL-UNNAMED --add-opens
+ * jdk.management/com.sun.management.internal=ALL-UNNAMED --add-opens
+ * java.base/java.math=ALL-UNNAMED --add-opens java.base/sun.reflect.annotation=ALL-UNNAMED
+ * --add-exports java.desktop/sun.awt=ALL-UNNAMED --add-exports java.desktop/sun.font=ALL-UNNAMED
+ * --add-opens java.desktop/sun.awt=ALL-UNNAMED --add-opens java.desktop/sun.font=ALL-UNNAMED
  *
  * @author shuigedeng
  * @version 2022.03
  * @since 2020/4/29 15:13
  */
-@EnableJpaRepositories(basePackages = {"com.taotao.cloud.auth.biz.repository", "com.taotao.cloud.auth.biz.idserver.repository"})
+@EnableJpaRepositories(basePackages = {"com.taotao.cloud.auth.biz.repository",
+	"com.taotao.cloud.auth.biz.idserver.repository"})
 @EnableFeignClients(basePackages = {"com.taotao.cloud.*.api.feign"})
 @EnableEncryptableProperties
 @EnableDiscoveryClient
@@ -70,19 +55,27 @@ public class TaoTaoCloudAuthApplication {
 	public static void main(String[] args) {
 		setNacosProperty();
 
+		String s = "b端用户 -> 用户+密码登录 手机号码+短信登录 用户+密码+验证码登录";
+
+		String s1 = "c端用户之pc端 -> 用户+密码登录 手机扫码登录 手机号码+短信登录 第三方登录(qq登录 微信登录 支付宝登录 github/gitee/weibo/抖音/钉钉/gitlab 等等)";
+		String s2 = "c端用户之小程序 -> 微信一键登录 手机号码+短信登录";
+		String s3 = "c端用户之app -> 短信密码登录 本机号码一键登录(不需要密码) 手机号码+短信登录 指纹登录 面部识别登录 手势登录 第三方登录(qq登录 微信登录 支付宝登录)";
+
 		SpringApplication.run(TaoTaoCloudAuthApplication.class, args);
 	}
 
 
-	public static void setNacosProperty(){
+	public static void setNacosProperty() {
 		/**
 		 * 设置nacos客户端日志和快照目录
 		 *
 		 * @see LocalConfigInfoProcessor
 		 */
 		String userHome = System.getProperty("user.home");
-		System.setProperty("JM.LOG.PATH", userHome + File.separator + "logs" + File.separator + "taotao-cloud-auth");
-		System.setProperty("JM.SNAPSHOT.PATH", userHome + File.separator + "logs" + File.separator + "taotao-cloud-auth");
+		System.setProperty("JM.LOG.PATH",
+			userHome + File.separator + "logs" + File.separator + "taotao-cloud-auth");
+		System.setProperty("JM.SNAPSHOT.PATH",
+			userHome + File.separator + "logs" + File.separator + "taotao-cloud-auth");
 		System.setProperty("nacos.logging.default.config.enabled", "false");
 	}
 }
