668ce6c003666b89ca8469c33b703f5ccfd19835
shuigedeng <981376577@qq.com>
2022-09-20 16:03:29 +0800

    [taotao-cloud-project-1723] update[ gradle build ]

--- taotao-cloud-microservice/taotao-cloud-sys/taotao-cloud-sys-biz/src/main/java/com/taotao/cloud/sys/biz/event/transactional/TransactionCommitSendMQListener.java
+++ taotao-cloud-microservice/taotao-cloud-sys/taotao-cloud-sys-biz/src/main/java/com/taotao/cloud/sys/biz/event/transactional/TransactionCommitSendMQListener.java
@@ -1,9 +1,6 @@
 package com.taotao.cloud.sys.biz.event.transactional;
 
-import com.taotao.cloud.stream.framework.rocketmq.RocketmqSendCallbackBuilder;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.rocketmq.spring.core.RocketMQTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -18,16 +15,15 @@ public class TransactionCommitSendMQListener {
 	/**
 	 * rocketMq
 	 */
-	@Autowired
-	private RocketMQTemplate rocketMQTemplate;
-
+	//@Autowired
+	//private RocketMQTemplate rocketMQTemplate;
 	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
 	public void send(TransactionCommitSendMQEvent event) {
 		log.info("事务提交，发送mq信息!{}", event);
 		String destination = event.getTopic() + ":" + event.getTag();
 		//发送订单变更mq消息
-		rocketMQTemplate.asyncSend(destination, event.getMessage(),
-			RocketmqSendCallbackBuilder.commonCallback());
+		//rocketMQTemplate.asyncSend(destination, event.getMessage(),
+		//	RocketmqSendCallbackBuilder.commonCallback());
 	}
 
 
