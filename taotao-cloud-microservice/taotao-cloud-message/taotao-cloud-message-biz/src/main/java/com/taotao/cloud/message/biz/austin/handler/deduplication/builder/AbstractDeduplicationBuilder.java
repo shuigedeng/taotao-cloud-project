8767f4e6edb605a197e75a15acbd450975f029b5
shuigedeng <981376577@qq.com>
2022-10-09 16:39:52 +0800

    [taotao-cloud-project-1816] update[ austin builder ]

---
+++ taotao-cloud-microservice/taotao-cloud-message/taotao-cloud-message-biz/src/main/java/com/taotao/cloud/message/biz/austin/handler/deduplication/builder/AbstractDeduplicationBuilder.java
@@ -0,0 +1,41 @@
+package com.taotao.cloud.message.biz.austin.handler.deduplication.builder;
+
+import com.alibaba.fastjson.JSONObject;
+import com.taotao.cloud.message.biz.austin.common.domain.TaskInfo;
+import com.taotao.cloud.message.biz.austin.handler.deduplication.DeduplicationHolder;
+import com.taotao.cloud.message.biz.austin.handler.deduplication.DeduplicationParam;
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @author 3y
+ * @date 2022/1/19
+ */
+public abstract class AbstractDeduplicationBuilder implements Builder {
+
+	protected Integer deduplicationType;
+
+	@Autowired
+	private DeduplicationHolder deduplicationHolder;
+
+	@PostConstruct
+	public void init() {
+		deduplicationHolder.putBuilder(deduplicationType, this);
+	}
+
+	public DeduplicationParam getParamsFromConfig(Integer key, String duplicationConfig,
+		TaskInfo taskInfo) {
+		JSONObject object = JSONObject.parseObject(duplicationConfig);
+		if (object == null) {
+			return null;
+		}
+		DeduplicationParam deduplicationParam = JSONObject.parseObject(
+			object.getString(DEDUPLICATION_CONFIG_PRE + key), DeduplicationParam.class);
+		if (deduplicationParam == null) {
+			return null;
+		}
+		deduplicationParam.setTaskInfo(taskInfo);
+		return deduplicationParam;
+	}
+
+}
---
+++ taotao-cloud-microservice/taotao-cloud-message/taotao-cloud-message-biz/src/main/java/com/taotao/cloud/message/biz/austin/handler/deduplication/builder/ContentDeduplicationBuilder.java
@@ -0,0 +1,32 @@
+package com.taotao.cloud.message.biz.austin.handler.deduplication.builder;
+
+import com.taotao.cloud.message.biz.austin.common.domain.TaskInfo;
+import com.taotao.cloud.message.biz.austin.common.enums.AnchorState;
+import com.taotao.cloud.message.biz.austin.common.enums.DeduplicationType;
+import com.taotao.cloud.message.biz.austin.handler.deduplication.DeduplicationParam;
+import org.springframework.stereotype.Service;
+
+
+/**
+ * @author huskey
+ * @date 2022/1/18
+ */
+@Service
+public class ContentDeduplicationBuilder extends AbstractDeduplicationBuilder implements Builder {
+
+	public ContentDeduplicationBuilder() {
+		deduplicationType = DeduplicationType.CONTENT.getCode();
+	}
+
+	@Override
+	public DeduplicationParam build(String deduplication, TaskInfo taskInfo) {
+		DeduplicationParam deduplicationParam = getParamsFromConfig(deduplicationType,
+			deduplication, taskInfo);
+		if (deduplicationParam == null) {
+			return null;
+		}
+		deduplicationParam.setAnchorState(AnchorState.CONTENT_DEDUPLICATION);
+		return deduplicationParam;
+
+	}
+}
