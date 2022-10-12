a72e347a87d701f40ba73075d55b013335499181
shuigedeng <981376577@qq.com>
2022-10-12 21:05:36 +0800

    [taotao-cloud-project-1821] update[ script logger  ]

---
+++ install/docker-compose/install.sh
@@ -0,0 +1,3 @@
+sudo curl -L https://get.daocloud.io/docker/compose/releases/download/1.25.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
