fdb79c687e8522f9991a66b222f23ba8b659c164
shuigedeng <981376577@qq.com>
2022-10-20 11:57:29 +0800

    [taotao-cloud-project-1822] build[ taotao-cloud-docker-image taotao-cloud-gateway-springcloud  ]

--- taotao-cloud-microservice/taotao-cloud-gateway/taotao-cloud-gateway-springcloud/src/main/java/com/taotao/cloud/gateway/springcloud/TaoTaoCloudGatewayApplication.java
+++ taotao-cloud-microservice/taotao-cloud-gateway/taotao-cloud-gateway-springcloud/src/main/java/com/taotao/cloud/gateway/springcloud/TaoTaoCloudGatewayApplication.java
@@ -37,6 +37,7 @@ public class TaoTaoCloudGatewayApplication {
 		SpringApplication.run(TaoTaoCloudGatewayApplication.class, args);
 	}
 
+	
 	public static void setNacosProperty() {
 		/**
 		 * 设置nacos客户端日志和快照目录
