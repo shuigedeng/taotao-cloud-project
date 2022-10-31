0f54f8d82fd1dd398e9f21015719c4b4f18f6d48
shuigedeng <981376577@qq.com>
2022-10-31 08:55:58 +0800

    [taotao-cloud-project-1631] update[ admin to monitor]

--- taotao-cloud-bigdata/taotao-cloud-spark/src/main/scala/com/taotao/cloud/bigdata/spark/ScalaWordCount.scala
+++ taotao-cloud-bigdata/taotao-cloud-spark/src/main/scala/com/taotao/cloud/bigdata/spark/ScalaWordCount.scala
@@ -37,14 +37,14 @@ import org.apache.spark.{SparkConf, SparkContext}
 --executor-memory 1g \
 --executor-cores 2 \
 --queue default \
-/opt/bigdata/spark-3.0.0-bin-hadoop3.2/jar/taotao-cloud-spark-2022.10.jar \
+/opt/bigdata/spark-3.0.0-bin-hadoop3.2/jar/taotao-cloud-spark-2022.11.jar \
 /opt/spark/input /opt/spark/output
 *   <p>
 *
 *
 *
 * @author shuigedeng
-* @version 2022.10
+* @version 2022.11
 * @since 2022 -07-21 10:45:19
 */
 object ScalaWordCount {
