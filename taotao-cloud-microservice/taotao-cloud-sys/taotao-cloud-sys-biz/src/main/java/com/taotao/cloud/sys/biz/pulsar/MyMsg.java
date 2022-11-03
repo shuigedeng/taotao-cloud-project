c60310f8baa04838938d5e9f6872b6ff8708f241
shuigedeng <981376577@qq.com>
2022-11-03 13:40:58 +0800

    [taotao-cloud-project-1631] update[ admin to monitor]

---
+++ taotao-cloud-microservice/taotao-cloud-sys/taotao-cloud-sys-biz/src/main/java/com/taotao/cloud/sys/biz/pulsar/MyMsg.java
@@ -0,0 +1,17 @@
+// package com.taotao.cloud.sys.biz.pulsar;
+//
+// public class MyMsg {
+//
+// 	private String data;
+//
+// 	public MyMsg(String data) {
+// 		this.data = data;
+// 	}
+//
+// 	public MyMsg() {
+// 	}
+//
+// 	public String getData() {
+// 		return data;
+// 	}
+// }
---
+++ taotao-cloud-microservice/taotao-cloud-sys/taotao-cloud-sys-biz/src/main/java/com/taotao/cloud/sys/biz/pulsar/PulsarConsumerInterceptor.java
@@ -0,0 +1,15 @@
+// package com.taotao.cloud.sys.biz.pulsar;
+//
+// import io.github.majusko.pulsar.consumer.DefaultConsumerInterceptor;
+// import org.apache.pulsar.client.api.Consumer;
+// import org.apache.pulsar.client.api.Message;
+// import org.springframework.stereotype.Component;
+//
+// @Component
+// public class PulsarConsumerInterceptor extends DefaultConsumerInterceptor<Object> {
+// 	@Override
+// 	public Message beforeConsume(Consumer<Object> consumer, Message<Object> message) {
+// 		System.out.println("do something");
+// 		return super.beforeConsume(consumer, message);
+// 	}
+// }
