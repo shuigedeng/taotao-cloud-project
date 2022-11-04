76cf898cd5ead6705c58c759f2cff12f441fe5a1
shuigedeng <981376577@qq.com>
2022-11-04 17:58:04 +0800

    [taotao-cloud-project-1822] add[ feignapi businessapi  ]

--- taotao-cloud-microservice/taotao-cloud-auth/taotao-cloud-auth-api/src/main/java/com/taotao/cloud/auth/api/tmp/service/mapper/UserOauth2Mapper.java
+++
@@ -1,8 +0,0 @@
-//package com.taotao.cloud.oauth2.biz.service.mapper;
-//
-//
-//import com.taotao.cloud.oauth2.biz.AuthUserOauth2;
-//
-//public interface UserOauth2Mapper extends BaseSuperMapper<AuthUserOauth2> {
-//
-//}
