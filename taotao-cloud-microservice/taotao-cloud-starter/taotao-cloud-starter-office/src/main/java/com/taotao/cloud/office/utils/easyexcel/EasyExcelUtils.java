bb8ed9ce98b713e6b9ff230632fad7ccd2b119cd
shuigedeng <981376577@qq.com>
2022-11-21 16:05:13 +0800

    [taotao-cloud-project-1822] add[ feignapi businessapi  ]

--- taotao-cloud-microservice/taotao-cloud-starter/taotao-cloud-starter-office/src/main/java/com/taotao/cloud/office/utils/easyexcel/EasyExcelUtils.java
+++ taotao-cloud-microservice/taotao-cloud-starter/taotao-cloud-starter-office/src/main/java/com/taotao/cloud/office/utils/easyexcel/EasyExcelUtils.java
@@ -9,6 +9,9 @@ import com.alibaba.excel.write.builder.ExcelWriterSheetBuilder;
 import com.alibaba.excel.write.metadata.WriteSheet;
 import com.alibaba.excel.write.metadata.fill.FillConfig;
 import com.alibaba.excel.write.metadata.fill.FillWrapper;
+import com.alibaba.excel.write.metadata.style.WriteCellStyle;
+import com.alibaba.excel.write.metadata.style.WriteFont;
+import com.alibaba.excel.write.style.HorizontalCellStyleStrategy;
 import com.alibaba.excel.write.style.column.LongestMatchColumnWidthStyleStrategy;
 import com.taotao.cloud.office.utils.easyexcel.convert.ExcelBigNumberConvert;
 import com.taotao.cloud.office.utils.easyexcel.core.CellMergeStrategy;
@@ -24,12 +27,22 @@ import java.util.Map;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.ss.usermodel.BorderStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.IndexedColors;
 
 /**
  * Excel相关处理
  *
  * @author shuigedeng
  * @version 2022.06
+ * @ExcelIgnore：忽略掉该字段；
+ * @ExcelProperty("用户名")：设置该列的名称为”用户名“；
+ * @ColumnWidth(20)：设置表格列的宽度为20；
+ * @DateTimeFormat("yyyy-MM-dd")：按照指定的格式对日期进行格式化；
+ * @ExcelProperty(value = "性别", converter =
+ * GenderConverter.class)：自定义内容转换器，类似枚举的实现，将“男”、“女”转换成“0”、“1”的数值。
  * @since 2022-07-31 20:58:12
  */
 public class EasyExcelUtils {
@@ -107,6 +120,10 @@ public class EasyExcelUtils {
 				.registerWriteHandler(new LongestMatchColumnWidthStyleStrategy())
 				// 大数值自动转换 防止失真
 				.registerConverter(new ExcelBigNumberConvert())
+				//// 注册通用格式策略
+				//.registerWriteHandler(CommonCellStyleStrategy.getHorizontalCellStyleStrategy())
+				//// 设置自定义格式策略
+				//.registerWriteHandler(new CustomCellWriteHandler())
 				.sheet(sheetName);
 			if (merge) {
 				// 合并处理器
@@ -267,6 +284,51 @@ public class EasyExcelUtils {
 	 */
 	public static String encodingFilename(String filename) {
 		return IdUtil.fastSimpleUUID() + "_" + filename + ".xlsx";
+	}
+
+	/**
+	 * 设置单元格样式(仅用于示例)
+	 *
+	 * @return 样式策略
+	 */
+	public static HorizontalCellStyleStrategy getHorizontalCellStyleStrategy() {
+		// 表头策略
+		WriteCellStyle headerCellStyle = new WriteCellStyle();
+		// 表头水平对齐居中
+		headerCellStyle.setHorizontalAlignment(HorizontalAlignment.CENTER);
+		// 背景色
+		headerCellStyle.setFillForegroundColor(IndexedColors.SKY_BLUE.getIndex());
+		WriteFont headerFont = new WriteFont();
+		headerFont.setFontHeightInPoints((short) 15);
+		headerCellStyle.setWriteFont(headerFont);
+		// 自动换行
+		headerCellStyle.setWrapped(Boolean.FALSE);
+		// 内容策略
+		WriteCellStyle contentCellStyle = new WriteCellStyle();
+		// 设置数据允许的数据格式,这里49代表所有可以都允许设置
+		//contentCellStyle.setDataFormat((short) 49);
+		// 设置背景色: 需要指定 FillPatternType 为FillPatternType.SOLID_FOREGROUND 不然无法显示背景颜色.头默认了 FillPatternType所以可以不指定
+		contentCellStyle.setFillPatternType(FillPatternType.SOLID_FOREGROUND);
+		contentCellStyle.setFillForegroundColor(IndexedColors.GREY_40_PERCENT.getIndex());
+		// 设置内容靠左对齐
+		contentCellStyle.setHorizontalAlignment(HorizontalAlignment.LEFT);
+		// 设置字体
+		WriteFont contentFont = new WriteFont();
+		contentFont.setFontHeightInPoints((short) 12);
+		contentCellStyle.setWriteFont(contentFont);
+		// 设置自动换行
+		contentCellStyle.setWrapped(Boolean.FALSE);
+		// 设置边框样式和颜色
+		contentCellStyle.setBorderLeft(BorderStyle.MEDIUM);
+		contentCellStyle.setBorderTop(BorderStyle.MEDIUM);
+		contentCellStyle.setBorderRight(BorderStyle.MEDIUM);
+		contentCellStyle.setBorderBottom(BorderStyle.MEDIUM);
+		contentCellStyle.setTopBorderColor(IndexedColors.RED.getIndex());
+		contentCellStyle.setBottomBorderColor(IndexedColors.GREEN.getIndex());
+		contentCellStyle.setLeftBorderColor(IndexedColors.YELLOW.getIndex());
+		contentCellStyle.setRightBorderColor(IndexedColors.ORANGE.getIndex());
+		// 将格式加入单元格样式策略
+		return new HorizontalCellStyleStrategy(headerCellStyle, contentCellStyle);
 	}
 
 }
