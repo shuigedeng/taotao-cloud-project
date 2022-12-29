5ea47286f49c9a91ab2ab43d6881b7bbb1b57c1f
shuigedeng <981376577@qq.com>
2022-12-29 13:31:01 +0800

    [ taotao-cloud-project-1561 ] update[ version to 2023.3.01 ]

--- doc/scripts/build_dockerfile.sh
+++ doc/scripts/build_dockerfile.sh
@@ -5,7 +5,7 @@ current_dir=`dirname $(pwd)`
 #JAVA_HOME="/opt/common/jdk-17"
 JAVA_HOME="/Users/dengtao/software/jdk-17.jdk/Contents/Home"
 
-VERSION=2022.12
+VERSION=2023.3.01
 
 function build_dockerfile() {
 	microservice_dir=$current_dir/taotao-cloud-microservice
