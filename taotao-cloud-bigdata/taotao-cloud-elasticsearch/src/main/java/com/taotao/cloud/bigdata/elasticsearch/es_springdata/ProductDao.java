8a41758496d442ae0b59b38e8678e2ec953d6cc7
shuigedeng <981376577@qq.com>
2022-12-30 16:12:25 +0800

    [ taotao-cloud-project-1561 ] update[ version to 2023.3.01 ]

--- taotao-cloud-bigdata/taotao-cloud-elasticsearch/src/main/java/com/taotao/cloud/bigdata/elasticsearch/es_springdata/ProductDao.java
+++
@@ -1,8 +0,0 @@
-package com.taotao.cloud.bigdata.elasticsearch.es_springdata;
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductDao extends ElasticsearchRepository<Product,Long> {
-}
