bbc726f7d98c6fcc5d0d193e02b2f7cebf547cff
shuigedeng <981376577@qq.com>
2023-01-19 15:55:03 +0800

    [ taotao-cloud-project-1561 ] update[ version to 2023.01 ]

--- taotao-cloud-microservice/taotao-cloud-sys/taotao-cloud-sys-biz/src/main/java/com/taotao/cloud/sys/biz/model/entity/dict/Dict.java
+++ taotao-cloud-microservice/taotao-cloud-sys/taotao-cloud-sys-biz/src/main/java/com/taotao/cloud/sys/biz/model/entity/dict/Dict.java
@@ -38,6 +38,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.experimental.Accessors;
 import org.hibernate.Hibernate;
 
 /**
