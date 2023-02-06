e03457823afd738858e952bb1058830b73e53eb6
shuigedeng <981376577@qq.com>
2023-02-06 11:32:14 +0800

    [ taotao-cloud-project-1561 ] update[ version to 2023.02 ]

--- taotao-cloud-microservice/taotao-cloud-sys/taotao-cloud-sys-biz/src/main/java/com/taotao/cloud/sys/biz/repository/inf/IDeptRepository.java
+++ taotao-cloud-microservice/taotao-cloud-sys/taotao-cloud-sys-biz/src/main/java/com/taotao/cloud/sys/biz/repository/inf/IDeptRepository.java
@@ -18,6 +18,7 @@ package com.taotao.cloud.sys.biz.repository.inf;
 import com.taotao.cloud.sys.biz.model.entity.system.Dept;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 /**
  * IDeptRepository
@@ -27,7 +28,41 @@ import org.springframework.data.jpa.repository.Query;
  * @since 2021/10/13 22:50
  */
 public interface IDeptRepository extends JpaRepository<Dept, Long> {
+
 	@Query("select d from Dept d where d.version <> ?1")
 	Dept findByVersionNot(Integer version);
 
+	@Query("""
+		select h from dept h where
+		h.userId=:#{#dept.userId} and
+		h.tabName=:#{#dept.tabName} and
+		h.headerName=:#{#dept.headerName}
+		""")
+	Dept find(@Param("dept") Dept dept);
+	//
+	//@Query(value = """
+	//	  select m.* from mining_area as m
+	//			where 1 = 1
+	//			and if(:name != '' , m.name like :name , 1 = 1)
+	//			and if(IFNULL(:startDate, '') != '' , DATE(m.create_time) > DATE(:startDate) , 1 = 1 )
+	//			and if(IFNULL(:endDate, '') != '' , DATE(m.create_time) < DATE(:endDate) , 1 = 1 )
+	//			and if(IFNULL(:status, '') != '' , m.audit_status = :status , 1 = 1 )
+	//			and m.deleted = :deleted
+	//			and ( m.tree_code like :treeCode and m.audit_status = 3  )
+	//			or ( m.tree_code = :noLikeTreeCode  )
+	//	""",
+	//	countQuery = """
+	//		   select count(m.*) from mining_area as m
+	//					where 1 = 1
+	//					and if(:name != '' , m.name like :name , 1 = 1)
+	//					and if(IFNULL(:startDate, '') != '' , DATE(m.create_time) > DATE(:startDate) , 1 = 1 )
+	//					and if(IFNULL(:endDate, '') != '' , DATE(m.create_time) < DATE(:endDate) , 1 = 1 )
+	//					and if(IFNULL(:status, '') != '' , m.audit_status = :status , 1 = 1 )
+	//					and m.deleted = :deleted
+	//					and ( m.tree_code like :treeCode and m.audit_status = 3  )
+	//					or ( m.tree_code = :noLikeTreeCode  )
+	//		""",
+	//	nativeQuery = true)
+	//List<Dept> findPage(String name, Date startDate, Date endDate, Dept status,
+	//	String treeCode, String noLikeTreeCode, Integer deleted, Pageable pageable);
 }
