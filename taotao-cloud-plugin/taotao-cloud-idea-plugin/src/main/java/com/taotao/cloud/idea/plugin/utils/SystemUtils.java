41009cc3d63b83937a44c08ec567422e910ad892
shuigedeng <981376577@qq.com>
2023-02-20 16:23:59 +0800

    [ taotao-cloud-project-1561 ] update[ version to 2023.02 ]

--- taotao-cloud-plugin/taotao-cloud-idea-plugin/src/main/java/com/taotao/cloud/idea/plugin/utils/SystemUtils.java
+++ taotao-cloud-plugin/taotao-cloud-idea-plugin/src/main/java/com/taotao/cloud/idea/plugin/utils/SystemUtils.java
@@ -1,20 +1,19 @@
 package com.taotao.cloud.idea.plugin.utils;
 
-import java.awt.*;
+import java.awt.Toolkit;
 import java.awt.datatransfer.StringSelection;
 
 /**
  * SystemUtils
- *
- * @author zhangjiaxing created on 2020-04-14
  */
 public class SystemUtils {
 
-    private SystemUtils() {}
+	private SystemUtils() {
+	}
 
-    public static void copyToClipboard(String content) {
-        Toolkit.getDefaultToolkit()
-                .getSystemClipboard()
-                .setContents(new StringSelection(content), null);
-    }
+	public static void copyToClipboard(String content) {
+		Toolkit.getDefaultToolkit()
+			.getSystemClipboard()
+			.setContents(new StringSelection(content), null);
+	}
 }
