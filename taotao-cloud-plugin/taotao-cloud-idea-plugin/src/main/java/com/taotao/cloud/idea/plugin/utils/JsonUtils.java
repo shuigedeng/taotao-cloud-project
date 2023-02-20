7f49954275c5a7f6c0246061debabc0e8b5e8938
shuigedeng <n3nbhbn3ymb>
2023-02-20 16:23:59 +0800

    [ taotao-cloud-project-1561 ] update[ version to 2023.02 ]

--- taotao-cloud-plugin/taotao-cloud-idea-plugin/src/main/java/com/taotao/cloud/idea/plugin/utils/JsonUtils.java
+++ taotao-cloud-plugin/taotao-cloud-idea-plugin/src/main/java/com/taotao/cloud/idea/plugin/utils/JsonUtils.java
@@ -1,6 +1,5 @@
 package com.taotao.cloud.idea.plugin.utils;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.util.DefaultIndenter;
 import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -9,50 +8,50 @@ import java.io.IOException;
 
 /**
  * JsonUtil
- *
- * @author zhangjiaxing created on 2020-04-14
  */
 public class JsonUtils {
 
-    private JsonUtils() {
-    }
+	private JsonUtils() {
+	}
 
-    public static String formatJson(String jsonStr) throws IOException {
-        Object jsonObject = Holder.MAPPER.readValue(jsonStr, Object.class);
-        return Holder.MAPPER.writer(Holder.DEFAULT_PRETTY_PRINTER).writeValueAsString(jsonObject);
-    }
+	public static String formatJson(String jsonStr) throws IOException {
+		Object jsonObject = Holder.MAPPER.readValue(jsonStr, Object.class);
+		return Holder.MAPPER.writer(Holder.DEFAULT_PRETTY_PRINTER).writeValueAsString(jsonObject);
+	}
 
-    public static String minifyJson(String jsonStr) throws IOException {
-        Object jsonObject = Holder.MAPPER.readValue(jsonStr, Object.class);
-        return Holder.MAPPER.writeValueAsString(jsonObject);
-    }
+	public static String minifyJson(String jsonStr) throws IOException {
+		Object jsonObject = Holder.MAPPER.readValue(jsonStr, Object.class);
+		return Holder.MAPPER.writeValueAsString(jsonObject);
+	}
 
-    public static void verifyJson(String jsonStr) throws IOException {
-        Holder.MAPPER.readValue(jsonStr, Object.class);
-    }
+	public static void verifyJson(String jsonStr) throws IOException {
+		Holder.MAPPER.readValue(jsonStr, Object.class);
+	}
 
-    public static JsonNode read(String jsonStr) throws IOException {
-        return Holder.MAPPER.readTree(jsonStr);
-    }
+	public static JsonNode read(String jsonStr) throws IOException {
+		return Holder.MAPPER.readTree(jsonStr);
+	}
 
-    private static final class Holder {
-        public static final ObjectMapper MAPPER = new ObjectMapper();
-        public static final DefaultPrettyPrinter DEFAULT_PRETTY_PRINTER = new CustomPrettyPrinter();
-    }
+	private static final class Holder {
 
-    private static final class CustomPrettyPrinter extends DefaultPrettyPrinter {
+		public static final ObjectMapper MAPPER = new ObjectMapper();
+		public static final DefaultPrettyPrinter DEFAULT_PRETTY_PRINTER = new CustomPrettyPrinter();
+	}
 
-        private static final DefaultIndenter UNIX_LINE_FEED_INSTANCE = new DefaultIndenter("  ", "\n");
+	private static final class CustomPrettyPrinter extends DefaultPrettyPrinter {
 
-        public CustomPrettyPrinter() {
-            super._objectFieldValueSeparatorWithSpaces = ": ";
-            super._objectIndenter = UNIX_LINE_FEED_INSTANCE;
-            super._arrayIndenter = UNIX_LINE_FEED_INSTANCE;
-        }
+		private static final DefaultIndenter UNIX_LINE_FEED_INSTANCE = new DefaultIndenter("  ",
+			"\n");
 
-        @Override
-        public DefaultPrettyPrinter createInstance() {
-            return new CustomPrettyPrinter();
-        }
-    }
+		public CustomPrettyPrinter() {
+			super._objectFieldValueSeparatorWithSpaces = ": ";
+			super._objectIndenter = UNIX_LINE_FEED_INSTANCE;
+			super._arrayIndenter = UNIX_LINE_FEED_INSTANCE;
+		}
+
+		@Override
+		public DefaultPrettyPrinter createInstance() {
+			return new CustomPrettyPrinter();
+		}
+	}
 }
