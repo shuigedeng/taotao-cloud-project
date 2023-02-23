31999f6632fb7423c56b0dc5a9b39950fc7afe2e
shuigedeng <981376577@qq.com>
2023-02-23 11:21:05 +0800

    [ taotao-cloud-project-1561 ] update[ version to 2023.02 ]

---
+++ taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-tenant/taotao-cloud-tenant-biz/src/main/java/com/taotao/cloud/report/biz/dao/TenantDatasourceConfigMapper.java
@@ -0,0 +1,18 @@
+package com.taotao.cloud.report.biz.dao;
+
+import com.baomidou.mybatisplus.annotation.InterceptorIgnore;
+import org.springframework.stereotype.Repository;
+import top.tangyh.basic.base.mapper.SuperMapper;
+import top.tangyh.lamp.tenant.entity.TenantDatasourceConfig;
+
+/**
+ * 租户数据源关系 Mapper
+ *
+ * @author zuihou
+ * @date 2020/8/27 下午4:48
+ */
+@Repository
+@InterceptorIgnore(tenantLine = "true", dynamicTableName = "true")
+public interface TenantDatasourceConfigMapper extends SuperMapper<TenantDatasourceConfig> {
+
+}
