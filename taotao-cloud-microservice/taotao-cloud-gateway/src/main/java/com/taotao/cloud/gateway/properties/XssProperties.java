b92636031e4b4205791c4698fea33bf181c07dcb
shuigedeng <981376577@qq.com>
2023-03-07 13:32:00 +0800

    修改视频异常数据信息查询

---
+++ taotao-cloud-microservice/taotao-cloud-gateway/src/main/java/com/taotao/cloud/gateway/properties/XssProperties.java
@@ -0,0 +1,27 @@
+package com.taotao.cloud.gateway.properties;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RefreshScope
+@ConfigurationProperties(prefix = "security.xss")
+@Data
+public class XssProperties
+{
+    /**
+     * Xss开关
+     */
+    private Boolean enabled;
+
+    /**
+     * 排除路径
+     */
+    private List<String> excludeUrls = new ArrayList<>();
+
+}
