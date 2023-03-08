d052b1768cff9d3516a6b564447baf3bbfad502c
shuigedeng <981376577@qq.com>
2023-03-08 18:02:07 +0800

    [ taotao-cloud-project-1605 ] update[ version to 2023.03 ]

--- taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-sys/taotao-cloud-sys-biz/src/main/java/com/taotao/cloud/sys/biz/service/grpc/DeviceGrpcServerService.java
+++ taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-sys/taotao-cloud-sys-biz/src/main/java/com/taotao/cloud/sys/biz/service/grpc/DeviceGrpcServerService.java
@@ -1,10 +1,16 @@
 package com.taotao.cloud.sys.biz.service.grpc;
 
+import com.taotao.cloud.common.support.cron.util.DateUtil;
+import com.taotao.cloud.sys.api.grpc.lib.DeviceFixServiceGrpc;
+import com.taotao.cloud.sys.api.grpc.lib.booleanReply;
+import com.taotao.cloud.sys.api.grpc.lib.deviceFix;
+import io.grpc.stub.StreamObserver;
 import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.service.GrpcService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Slf4j
-@GrpcService(DeviceFixServiceGrpc.class)
+@GrpcService
 public class DeviceGrpcServerService extends DeviceFixServiceGrpc.DeviceFixServiceImplBase {
 
 	@Autowired
