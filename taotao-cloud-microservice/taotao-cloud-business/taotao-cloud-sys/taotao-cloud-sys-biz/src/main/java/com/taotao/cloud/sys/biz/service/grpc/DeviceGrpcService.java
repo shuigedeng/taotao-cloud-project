fb1f8490ce5237f4ce72f002892286f49a2d924c
shuigedeng <981376577@qq.com>
2023-03-09 15:55:02 +0800

    [ taotao-cloud-project-1605 ] update[ version to 2023.03 ]

--- taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-sys/taotao-cloud-sys-biz/src/main/java/com/taotao/cloud/sys/biz/service/grpc/DeviceGrpcService.java
+++ taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-sys/taotao-cloud-sys-biz/src/main/java/com/taotao/cloud/sys/biz/service/grpc/DeviceGrpcService.java
@@ -1,9 +1,7 @@
 package com.taotao.cloud.sys.biz.service.grpc;
 
-import com.taotao.cloud.sys.api.grpc.lib.DeviceFixServiceGrpc;
-import com.taotao.cloud.sys.api.grpc.lib.MyServiceGrpc.MyServiceBlockingStub;
-import com.taotao.cloud.sys.api.grpc.lib.booleanReply;
-import com.taotao.cloud.sys.api.grpc.lib.deviceFix;
+import com.taotao.cloud.sys.api.grpc.BooleanReply;
+import com.taotao.cloud.sys.api.grpc.MyServiceGrpc.MyServiceBlockingStub;
 import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.stereotype.Service;
@@ -16,18 +14,19 @@ public class DeviceGrpcService {
 	private MyServiceBlockingStub myServiceStub;
 
 	public String insertDeviceFix() {
-		DeviceFixServiceGrpc.DeviceFixServiceBlockingStub stub = DeviceFixServiceGrpc.newBlockingStub(
-			serverChannel);
-		booleanReply response = stub.insertDeviceFix(
-			deviceFix.newBuilder()
-				.setId("UUID-O1")
-				.setSerialNum("AUCCMA-01")
-				.setAddress("SHENZHEN")
-				.setCreatetime(DateUtil.toString(new Date(), DatePattern.TIMESTAMP))
-				.setUpdatetime(DateUtil.toString(new Date(), DatePattern.TIMESTAMP))
-				.setStatus(1)
-				.setType(1)
-				.build());
+		//DeviceFixServiceGrpc.DeviceFixServiceBlockingStub stub = DeviceFixServiceGrpc.newBlockingStub(
+		//	serverChannel);
+		//booleanReply response = stub.insertDeviceFix(
+		//	deviceFix.newBuilder()
+		//		.setId("UUID-O1")
+		//		.setSerialNum("AUCCMA-01")
+		//		.setAddress("SHENZHEN")
+		//		.setCreatetime(DateUtil.toString(new Date(), DatePattern.TIMESTAMP))
+		//		.setUpdatetime(DateUtil.toString(new Date(), DatePattern.TIMESTAMP))
+		//		.setStatus(1)
+		//		.setType(1)
+		//		.build());
+		BooleanReply response = null;
 		log.info("grpc消费者收到：--》" + response.getReply());
 		if (response.getReply()) {
 			return "success";
