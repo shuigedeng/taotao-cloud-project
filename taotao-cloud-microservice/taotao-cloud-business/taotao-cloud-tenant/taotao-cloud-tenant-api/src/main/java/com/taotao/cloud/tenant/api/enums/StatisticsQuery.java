2467a1980df15f18e56f56fa959320ae505a0ca5
shuigedeng <981376577@qq.com>
2023-03-10 13:14:18 +0800

    [ taotao-cloud-project-1605 ] update[ version to 2023.03 ]

---
+++ taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-tenant/taotao-cloud-tenant-api/src/main/java/com/taotao/cloud/tenant/api/enums/StatisticsQuery.java
@@ -0,0 +1,17 @@
+package com.taotao.cloud.tenant.api.enums;
+
+/**
+ * 统计查询
+ */
+public enum StatisticsQuery {
+
+	/**
+	 * 数量
+	 */
+	NUM,
+
+	/**
+	 * 金额
+	 */
+	PRICE,
+}
---
+++ taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-tenant/taotao-cloud-tenant-api/src/main/java/com/taotao/cloud/tenant/api/model/vo/StoreStatisticsDataVO.java
@@ -0,0 +1,30 @@
+package com.taotao.cloud.tenant.api.model.vo;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 商品统计VO
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class StoreStatisticsDataVO {
+
+	@Schema(description = "店铺ID")
+	private String storeId;
+
+	@Schema(description = "店铺名称")
+	private String storeName;
+
+	@Schema(description = "销售数量")
+	private String num;
+
+	@Schema(description = "销售金额")
+	private BigDecimal price;
+}
