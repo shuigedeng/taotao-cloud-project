5883d09cd4b49bc7adcaf1ff1d1a746311774e06
shuigedeng <981376577@qq.com>
2023-03-14 09:13:50 +0800

    asf

---
+++ taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-flowable/taotao-cloud-flowable-biz/src/main/java/com/taotao/cloud/flowable/biz/flowable/common/constant/ProcessConstants.java
@@ -0,0 +1,80 @@
+package com.taotao.cloud.flowable.biz.flowable.common.constant;
+
+/**
+ * 流程常量信息
+ *
+ * @author Tony
+ * @date 2021/4/17 22:46
+ */
+public class ProcessConstants {
+
+    /**
+     * 动态数据
+     */
+    public static final String DYNAMIC = "dynamic";
+
+    /**
+     * 固定任务接收
+     */
+    public static final String FIXED = "fixed";
+
+    /**
+     * 单个审批人
+     */
+    public static final String ASSIGNEE = "assignee";
+
+
+    /**
+     * 候选人
+     */
+    public static final String CANDIDATE_USERS = "candidateUsers";
+
+
+    /**
+     * 审批组
+     */
+    public static final String CANDIDATE_GROUPS = "candidateGroups";
+
+    /**
+     * 单个审批人
+     */
+    public static final String PROCESS_APPROVAL = "approval";
+
+    /**
+     * 会签人员
+     */
+    public static final String PROCESS_MULTI_INSTANCE_USER = "userList";
+
+    /**
+     * nameapace
+     */
+    public static final String NAMASPASE = "http://flowable.org/bpmn";
+
+    /**
+     * 会签节点
+     */
+    public static final String PROCESS_MULTI_INSTANCE = "multiInstance";
+
+    /**
+     * 自定义属性 dataType
+     */
+    public static final String PROCESS_CUSTOM_DATA_TYPE = "dataType";
+
+    /**
+     * 自定义属性 userType
+     */
+    public static final String PROCESS_CUSTOM_USER_TYPE = "userType";
+
+    /**
+     * 初始化人员
+     */
+    public static final String PROCESS_INITIATOR = "INITIATOR";
+
+
+    /**
+     * 流程跳过
+     */
+    public static final String FLOWABLE_SKIP_EXPRESSION_ENABLED = "_FLOWABLE_SKIP_EXPRESSION_ENABLED";
+
+
+}
---
+++ taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-flowable/taotao-cloud-flowable-biz/src/main/java/com/taotao/cloud/flowable/biz/flowable/domain/vo/FlowTaskVo.java
@@ -0,0 +1,56 @@
+package com.taotao.cloud.flowable.biz.flowable.domain.vo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * <p>流程任务<p>
+ *
+ * @author Tony
+ * @date 2021-04-03
+ */
+@Data
+@ApiModel("工作流任务相关--请求参数")
+public class FlowTaskVo {
+
+    @ApiModelProperty("任务Id")
+    private String taskId;
+
+    @ApiModelProperty("用户Id")
+    private String userId;
+
+    @ApiModelProperty("任务意见")
+    private String comment;
+
+    @ApiModelProperty("流程实例Id")
+    private String instanceId;
+
+    @ApiModelProperty("节点")
+    private String targetKey;
+
+    private String deploymentId;
+    @ApiModelProperty("流程环节定义ID")
+    private String defId;
+
+    @ApiModelProperty("子执行流ID")
+    private String currentChildExecutionId;
+
+    @ApiModelProperty("子执行流是否已执行")
+    private Boolean flag;
+
+    @ApiModelProperty("流程变量信息")
+    private Map<String, Object> variables;
+
+    @ApiModelProperty("审批人")
+    private String assignee;
+
+    @ApiModelProperty("候选人")
+    private List<String> candidateUsers;
+
+    @ApiModelProperty("审批组")
+    private List<String> candidateGroups;
+}
