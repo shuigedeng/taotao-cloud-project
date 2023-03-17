fa4299c4e2f8a7de0efeef81468d32e161e27c5b
shuigedeng <981376577@qq.com>
2023-03-17 08:41:43 +0800

    [ taotao-cloud-project-1605 ] update[ version to 2023.03 ]

--- taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-job/taotao-cloud-job-biz/src/main/java/com/taotao/cloud/job/biz/schedule1/runner/TaskApplicationDestroy.java
+++ taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-job/taotao-cloud-job-biz/src/main/java/com/taotao/cloud/job/biz/schedule1/runner/TaskApplicationDestroy.java
@@ -1,27 +1,27 @@
-package com.taotao.cloud.job.biz.schedule1.runner;
-
-import com.taotao.cloud.job.schedule.schedule1.task.TaskManager;
-import jakarta.annotation.Resource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.stereotype.Component;
-
-/**
- * 启动完成后加载
- **/
-@Component
-public class TaskApplicationDestroy implements DisposableBean {
-
-	private final Logger logger = LoggerFactory.getLogger(TaskApplicationRunner.class);
-
-	@Resource
-	private TaskManager taskManager;
-
-	@Override
-	public void destroy() throws Exception {
-		logger.info("==== 系统运行结束 ====");
-		taskManager.destroyTask();
-	}
-
-}
+//package com.taotao.cloud.job.biz.schedule1.runner;
+//
+//import com.taotao.cloud.job.schedule.schedule1.task.TaskManager;
+//import jakarta.annotation.Resource;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.DisposableBean;
+//import org.springframework.stereotype.Component;
+//
+///**
+// * 启动完成后加载
+// **/
+//@Component
+//public class TaskApplicationDestroy implements DisposableBean {
+//
+//	private final Logger logger = LoggerFactory.getLogger(TaskApplicationRunner.class);
+//
+//	@Resource
+//	private TaskManager taskManager;
+//
+//	@Override
+//	public void destroy() throws Exception {
+//		logger.info("==== 系统运行结束 ====");
+//		taskManager.destroyTask();
+//	}
+//
+//}
--- taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-job/taotao-cloud-job-biz/src/main/java/com/taotao/cloud/job/biz/schedule1/runner/TaskApplicationRunner.java
+++ taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-job/taotao-cloud-job-biz/src/main/java/com/taotao/cloud/job/biz/schedule1/runner/TaskApplicationRunner.java
@@ -1,29 +1,29 @@
-package com.taotao.cloud.job.biz.schedule1.runner;
-
-import com.taotao.cloud.job.schedule.schedule1.task.TaskManager;
-import jakarta.annotation.Resource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.stereotype.Component;
-
-/**
- * 启动完成后加载
- **/
-@Component
-public class TaskApplicationRunner implements ApplicationRunner {
-
-	@Resource
-	private TaskManager taskManager;
-
-	private final Logger logger = LoggerFactory.getLogger(TaskApplicationRunner.class);
-
-	@Override
-	public void run(ApplicationArguments args) {
-		logger.info("==== 系统运行开始 ====");
-
-		// 初始化任务
-		taskManager.initTask();
-	}
-}
+//package com.taotao.cloud.job.biz.schedule1.runner;
+//
+//import com.taotao.cloud.job.schedule.schedule1.task.TaskManager;
+//import jakarta.annotation.Resource;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.boot.ApplicationArguments;
+//import org.springframework.boot.ApplicationRunner;
+//import org.springframework.stereotype.Component;
+//
+///**
+// * 启动完成后加载
+// **/
+//@Component
+//public class TaskApplicationRunner implements ApplicationRunner {
+//
+//	@Resource
+//	private TaskManager taskManager;
+//
+//	private final Logger logger = LoggerFactory.getLogger(TaskApplicationRunner.class);
+//
+//	@Override
+//	public void run(ApplicationArguments args) {
+//		logger.info("==== 系统运行开始 ====");
+//
+//		// 初始化任务
+//		taskManager.initTask();
+//	}
+//}
