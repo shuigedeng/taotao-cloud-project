4bf472dc0de15734cf5eebebbd7e08d4d4d37a18
shuigedeng <n3nbhbn3ymb>
2023-04-23 11:10:20 +0800

    [ taotao-cloud-project-1702 ] update[ version to 2023.04 ]

--- taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-sys/taotao-cloud-sys-biz/src/main/java/com/taotao/cloud/sys/biz/controller/TestGrpcController.java
+++ taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-sys/taotao-cloud-sys-biz/src/main/java/com/taotao/cloud/sys/biz/controller/TestGrpcController.java
@@ -16,30 +16,30 @@
 
 package com.taotao.cloud.sys.biz.controller;
 
-import com.taotao.cloud.sys.biz.service.grpc.DeviceGrpcService;
-import org.apache.pulsar.shade.io.swagger.annotations.Api;
-import org.apache.pulsar.shade.io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@Api(value = "Testcontroller", description = "测试grpc")
-public class TestGrpcController {
-
-    @Autowired
-    private DeviceGrpcService deviceGrpcService;
-
-    @RequestMapping("/testInsertDeviceFix")
-    @ApiOperation(value = "test", httpMethod = "GET", notes = "测试grpc插入")
-    public String printMessage3(@RequestParam(defaultValue = "Hmemb") String name) {
-        return deviceGrpcService.insertDeviceFix();
-    }
-
-    @RequestMapping("/TEST1")
-    @ApiOperation(value = "test", httpMethod = "GET", notes = "测试1")
-    public String printMessage(@RequestParam(defaultValue = "Michael") String name) {
-        return name;
-    }
-}
+// import com.taotao.cloud.sys.biz.service.grpc.DeviceGrpcService;
+// import org.apache.pulsar.shade.io.swagger.annotations.Api;
+// import org.apache.pulsar.shade.io.swagger.annotations.ApiOperation;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.RestController;
+//
+// @RestController
+// @Api(value = "Testcontroller", description = "测试grpc")
+// public class TestGrpcController {
+//
+//     @Autowired
+//     private DeviceGrpcService deviceGrpcService;
+//
+//     @RequestMapping("/testInsertDeviceFix")
+//     @ApiOperation(value = "test", httpMethod = "GET", notes = "测试grpc插入")
+//     public String printMessage3(@RequestParam(defaultValue = "Hmemb") String name) {
+//         return deviceGrpcService.insertDeviceFix();
+//     }
+//
+//     @RequestMapping("/TEST1")
+//     @ApiOperation(value = "test", httpMethod = "GET", notes = "测试1")
+//     public String printMessage(@RequestParam(defaultValue = "Michael") String name) {
+//         return name;
+//     }
+// }
--- taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-sys/taotao-cloud-sys-biz/src/main/java/com/taotao/cloud/sys/biz/service/grpc/DeviceGrpcService.java
+++ taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-sys/taotao-cloud-sys-biz/src/main/java/com/taotao/cloud/sys/biz/service/grpc/DeviceGrpcService.java
@@ -16,39 +16,39 @@
 
 package com.taotao.cloud.sys.biz.service.grpc;
 
-import com.taotao.cloud.sys.api.grpc.BooleanReply;
-import com.taotao.cloud.sys.api.grpc.MyServiceGrpc.MyServiceBlockingStub;
-import lombok.extern.slf4j.Slf4j;
-import net.devh.boot.grpc.client.inject.GrpcClient;
-import org.springframework.stereotype.Service;
-
-@Service
-@Slf4j
-public class DeviceGrpcService {
-
-    @GrpcClient("myService")
-    private MyServiceBlockingStub myServiceStub;
-
-    public String insertDeviceFix() {
-        // DeviceFixServiceGrpc.DeviceFixServiceBlockingStub stub =
-        // DeviceFixServiceGrpc.newBlockingStub(
-        //	serverChannel);
-        // booleanReply response = stub.insertDeviceFix(
-        //	deviceFix.newBuilder()
-        //		.setId("UUID-O1")
-        //		.setSerialNum("AUCCMA-01")
-        //		.setAddress("SHENZHEN")
-        //		.setCreatetime(DateUtil.toString(new Date(), DatePattern.TIMESTAMP))
-        //		.setUpdatetime(DateUtil.toString(new Date(), DatePattern.TIMESTAMP))
-        //		.setStatus(1)
-        //		.setType(1)
-        //		.build());
-        BooleanReply response = null;
-        log.info("grpc消费者收到：--》" + response.getReply());
-        if (response.getReply()) {
-            return "success";
-        } else {
-            return "fail";
-        }
-    }
-}
+// import com.taotao.cloud.sys.api.grpc.BooleanReply;
+// import com.taotao.cloud.sys.api.grpc.MyServiceGrpc.MyServiceBlockingStub;
+// import lombok.extern.slf4j.Slf4j;
+// import net.devh.boot.grpc.client.inject.GrpcClient;
+// import org.springframework.stereotype.Service;
+//
+// @Service
+// @Slf4j
+// public class DeviceGrpcService {
+//
+//     @GrpcClient("myService")
+//     private MyServiceBlockingStub myServiceStub;
+//
+//     public String insertDeviceFix() {
+//         // DeviceFixServiceGrpc.DeviceFixServiceBlockingStub stub =
+//         // DeviceFixServiceGrpc.newBlockingStub(
+//         //	serverChannel);
+//         // booleanReply response = stub.insertDeviceFix(
+//         //	deviceFix.newBuilder()
+//         //		.setId("UUID-O1")
+//         //		.setSerialNum("AUCCMA-01")
+//         //		.setAddress("SHENZHEN")
+//         //		.setCreatetime(DateUtil.toString(new Date(), DatePattern.TIMESTAMP))
+//         //		.setUpdatetime(DateUtil.toString(new Date(), DatePattern.TIMESTAMP))
+//         //		.setStatus(1)
+//         //		.setType(1)
+//         //		.build());
+//         BooleanReply response = null;
+//         log.info("grpc消费者收到：--》" + response.getReply());
+//         if (response.getReply()) {
+//             return "success";
+//         } else {
+//             return "fail";
+//         }
+//     }
+// }
