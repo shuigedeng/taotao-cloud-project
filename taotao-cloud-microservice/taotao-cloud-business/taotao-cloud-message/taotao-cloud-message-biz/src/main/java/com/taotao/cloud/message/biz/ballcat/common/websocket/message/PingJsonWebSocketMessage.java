da1799e38ecc9815fc88228025b43da66fc488dc
shuigedeng <981376577@qq.com>
2023-05-15 13:54:58 +0800

    aa

---
+++ taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-message/taotao-cloud-message-biz/src/main/java/com/taotao/cloud/message/biz/ballcat/common/websocket/message/PingJsonWebSocketMessage.java
@@ -0,0 +1,13 @@
+package com.taotao.cloud.message.biz.ballcat.common.websocket.message;
+
+/**
+ * @author Hccake 2021/1/4
+ * @version 1.0
+ */
+public class PingJsonWebSocketMessage extends JsonWebSocketMessage {
+
+	public PingJsonWebSocketMessage() {
+		super(WebSocketMessageTypeEnum.PING.getValue());
+	}
+
+}
