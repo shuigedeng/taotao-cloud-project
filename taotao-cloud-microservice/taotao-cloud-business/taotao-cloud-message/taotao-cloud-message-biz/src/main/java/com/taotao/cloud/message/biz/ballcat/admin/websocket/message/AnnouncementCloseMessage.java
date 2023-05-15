d35e0397553ff6ca063d21e72aec5b58c2866088
shuigedeng <981376577@qq.com>
2023-05-15 13:54:58 +0800

    aa

---
+++ taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-message/taotao-cloud-message-biz/src/main/java/com/taotao/cloud/message/biz/ballcat/admin/websocket/message/AnnouncementCloseMessage.java
@@ -0,0 +1,24 @@
+package com.taotao.cloud.message.biz.ballcat.admin.websocket.message;
+
+import com.hccake.ballcat.common.websocket.message.JsonWebSocketMessage;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author Hccake 2021/1/7
+ * @version 1.0
+ */
+@Getter
+@Setter
+public class AnnouncementCloseMessage extends JsonWebSocketMessage {
+
+	public AnnouncementCloseMessage() {
+		super("announcement-close");
+	}
+
+	/**
+	 * ID
+	 */
+	private Long id;
+
+}
---
+++ taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-message/taotao-cloud-message-biz/src/main/java/com/taotao/cloud/message/biz/ballcat/common/websocket/message/PongJsonWebSocketMessage.java
@@ -0,0 +1,13 @@
+package com.taotao.cloud.message.biz.ballcat.common.websocket.message;
+
+/**
+ * @author Hccake 2021/1/4
+ * @version 1.0
+ */
+public class PongJsonWebSocketMessage extends JsonWebSocketMessage {
+
+	public PongJsonWebSocketMessage() {
+		super(WebSocketMessageTypeEnum.PONG.getValue());
+	}
+
+}
