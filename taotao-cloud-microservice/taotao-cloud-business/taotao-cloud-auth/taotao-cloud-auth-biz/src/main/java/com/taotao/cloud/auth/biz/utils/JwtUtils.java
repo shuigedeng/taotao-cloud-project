7b1a01d580743f52b506cbc4f24d31f96e2dc438
shuigedeng <dengtao>
2023-07-12 11:36:38 +0800

    [ taotao-cloud-project-2012 ] update[ version to 2023.08 ]

--- taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-auth/taotao-cloud-auth-biz/src/main/java/com/taotao/cloud/auth/biz/utils/JwtUtils.java
+++ taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-auth/taotao-cloud-auth-biz/src/main/java/com/taotao/cloud/auth/biz/utils/JwtUtils.java
@@ -88,4 +88,7 @@ public class JwtUtils {
 
         return claimsBuilder;
     }
+
+
+
 }
--- taotao-cloud-microservice/taotao-cloud-gateway/src/main/java/com/taotao/cloud/gateway/properties/SecurityProperties.java
+++ taotao-cloud-microservice/taotao-cloud-gateway/src/main/java/com/taotao/cloud/gateway/properties/SecurityProperties.java
@@ -18,90 +18,92 @@ package com.taotao.cloud.gateway.properties;
 
 import com.taotao.cloud.common.utils.context.ContextUtils;
 import jakarta.annotation.PostConstruct;
-
-import java.util.*;
-import java.util.stream.Collectors;
-
 import org.dromara.hutool.core.collection.CollUtil;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 /**
  * 验证权限配置
  *
- * @date 2020-10-28
+ * @author shuigedeng
+ * @version 2023.07
+ * @since 2023-07-12 11:11:45
  */
 @RefreshScope
 @ConfigurationProperties(prefix = SecurityProperties.PREFIX)
 @AutoConfigureBefore(ApiProperties.class)
 public class SecurityProperties {
 
-    public static final String PREFIX = "taotao.cloud.gateway.security";
+	public static final String PREFIX = "taotao.cloud.gateway.security";
 
-    public static final String[] ENDPOINTS = {
-        "/actuator/**",
-        "/v3/**",
-        "/*/v3/**",
-        "/fallback",
-        "/favicon.ico",
-        "/startup-report",
-        "/swagger-resources/**",
-        "/webjars/**",
-        "/druid/**",
-        "/*/*.html",
-        "/*/*.css",
-        "/*/*.js",
-        "/*.js",
-        "/*.css",
-        "/*.html",
-        "/*/favicon.ico",
-        "/*/api-docs",
-        "/health/**",
-        "/css/**",
-        "/js/**",
-        "/k8s/**",
-        "/k8s",
-        "/doc.html",
-        "/tt/**",
-        "/images/**"
-    };
+	public static final String[] ENDPOINTS = {
+		"/actuator/**",
+		"/v3/**",
+		"/*/v3/**",
+		"/fallback",
+		"/favicon.ico",
+		"/startup-report",
+		"/swagger-resources/**",
+		"/webjars/**",
+		"/druid/**",
+		"/*/*.html",
+		"/*/*.css",
+		"/*/*.js",
+		"/*.js",
+		"/*.css",
+		"/*.html",
+		"/*/favicon.ico",
+		"/*/api-docs",
+		"/health/**",
+		"/css/**",
+		"/js/**",
+		"/k8s/**",
+		"/k8s",
+		"/doc.html",
+		"/tt/**",
+		"/images/**"
+	};
 
-    /**
-     * 是否启用网关鉴权模式
-     */
-    private Boolean enabled = true;
+	/**
+	 * 是否启用网关鉴权模式
+	 */
+	private Boolean enabled = true;
 
-    /**
-     * 忽略URL，List列表形式
-     */
-    private List<String> ignoreUrl = new ArrayList<>();
+	/**
+	 * 忽略URL，List列表形式
+	 */
+	private List<String> ignoreUrl = new ArrayList<>();
 
-    /**
-     * 首次加载合并ENDPOINTS
-     */
-    @PostConstruct
-    public void initIgnoreUrl() {
-        ApiProperties apiProperties = ContextUtils.getBean(ApiProperties.class, true);
-        if (Objects.nonNull(apiProperties)) {
-            String baseUri = apiProperties.getBaseUri();
-			ignoreUrl = ( List<String> )CollUtil.addAll(new ArrayList<>(ignoreUrl.stream().map(url -> baseUri + url).toList()), ENDPOINTS);
-        }
-    }
+	/**
+	 * 首次加载合并ENDPOINTS
+	 */
+	@PostConstruct
+	public void initIgnoreUrl() {
+		ApiProperties apiProperties = ContextUtils.getBean(ApiProperties.class, true);
+		if (Objects.nonNull(apiProperties)) {
+			String baseUri = apiProperties.getBaseUri();
+			ignoreUrl = (List<String>) CollUtil.addAll(new ArrayList<>(ignoreUrl.stream().map(url -> baseUri + url).toList()), ENDPOINTS);
+		}
+	}
 
-    public Boolean getEnabled() {
-        return enabled;
-    }
+	public Boolean getEnabled() {
+		return enabled;
+	}
 
-    public void setEnabled(Boolean enabled) {
-        this.enabled = enabled;
-    }
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
 
-    public List<String> getIgnoreUrl() {
-        return ignoreUrl;
-    }
+	public List<String> getIgnoreUrl() {
+		return ignoreUrl;
+	}
 
-    public void setIgnoreUrl(List<String> ignoreUrl) {
-        this.ignoreUrl = ignoreUrl;
-    }
+	public void setIgnoreUrl(List<String> ignoreUrl) {
+		this.ignoreUrl = ignoreUrl;
+	}
 }
