9c9fe3f2be03641e16f78f342d92c97a1a12349f
shuigedeng <dengtao>
2023-07-24 16:29:53 +0800

    [ taotao-cloud-project-2012 ] update[ version to 2023.08  ]

---
+++ taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-auth/taotao-cloud-auth-biz/src/main/java/com/taotao/cloud/auth/biz/authentication/federation/Oauth2ThirdAccountMapper.java
@@ -0,0 +1,13 @@
+package com.taotao.cloud.auth.biz.authentication.federation;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 三方登录账户信息表 Mapper 接口
+ * </p>
+ *
+ */
+public interface Oauth2ThirdAccountMapper extends BaseMapper<Oauth2ThirdAccount> {
+
+}
---
+++ taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-auth/taotao-cloud-auth-biz/src/main/java/com/taotao/cloud/auth/biz/authentication/federation/strategy/impl/GiteeUserConverter.java
@@ -0,0 +1,33 @@
+package com.taotao.cloud.auth.biz.authentication.federation.strategy.impl;
+
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+import static com.example.constant.SecurityConstants.THIRD_LOGIN_GITEE;
+
+/**
+ * 转换通过码云登录的用户信息
+ *
+ */
+@Component(THIRD_LOGIN_GITEE)
+public class GiteeUserConverter implements Oauth2UserConverterStrategy {
+
+
+    @Override
+    public Oauth2ThirdAccount convert(OAuth2User oAuth2User) {
+        // 获取三方用户信息
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+        // 转换至Oauth2ThirdAccount
+        Oauth2ThirdAccount thirdAccount = new Oauth2ThirdAccount();
+        thirdAccount.setUniqueId(oAuth2User.getName());
+        thirdAccount.setThirdUsername(String.valueOf(attributes.get("login")));
+        thirdAccount.setType(THIRD_LOGIN_GITEE);
+        thirdAccount.setBlog(String.valueOf(attributes.get("blog")));
+        // 设置基础用户信息
+        thirdAccount.setName(String.valueOf(attributes.get("name")));
+        thirdAccount.setAvatarUrl(String.valueOf(attributes.get("avatar_url")));
+        return thirdAccount;
+    }
+}
