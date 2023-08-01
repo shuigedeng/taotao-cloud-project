5eaba691ce4c7c30a2c399771c78efba98dce404
shuigedeng <dengtao>
2023-08-01 14:12:18 +0800

    [ taotao-cloud-project-2012 ] update[ version to 2023.09 taotao-cloud-sys taotao-cloud-xxl-job]

--- doc/install/taotao-cloud/taotao-cloud-order.sh
+++ doc/install/taotao-cloud/taotao-cloud-order.sh
@@ -37,7 +37,7 @@ JAVA_OPTS="
 -javaagent:/opt/cloud/apache-skywalking-apm-bin/agent/skywalking-agent.jar
 -Dskywalking.agent.service_name=${APP_NAME}
 -Dskywalking.agent.authentication=taotao-cloud
--Dskywalking.logging.file_name=${APP_NAME}.skywalking.log
+-Dskywalking.logging.file_name=skywalking.log
 -Dskywalking.logging.level=INFO
 -Dskywalking.logging.dir=${JAR_PATH}
 -Dskywalking.collector.backend_service=192.168.10.200:11800
