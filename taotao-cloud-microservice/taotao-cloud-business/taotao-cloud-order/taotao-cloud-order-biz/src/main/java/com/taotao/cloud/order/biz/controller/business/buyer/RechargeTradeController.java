101f32598b646edcb77501465a422aba56fcd195
shuigedeng <dengtao>
2023-08-02 14:38:41 +0800

    [ taotao-cloud-project-2012 ] update[ version to 2023.09]

--- taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-order/taotao-cloud-order-biz/src/main/java/com/taotao/cloud/order/biz/controller/business/buyer/RechargeTradeController.java
+++ taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-order/taotao-cloud-order-biz/src/main/java/com/taotao/cloud/order/biz/controller/business/buyer/RechargeTradeController.java
@@ -24,14 +24,14 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
 
 /**
  * 买家端,预存款充值记录API
@@ -45,19 +45,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Tag(name = "买家端-预存款充值记录API", description = "买家端-预存款充值记录API")
 @RequestMapping("/order/buyer/recharge")
-@Transactional(rollbackFor = Exception.class)
 public class RechargeTradeController {
 
-    private final IFeignMemberRechargeApi memberRechargeApi;
+	private final IFeignMemberRechargeApi memberRechargeApi;
 
-    @Operation(summary = "创建余额充值订单", description = "创建余额充值订单")
-    @RequestLogger
-    @PreAuthorize("hasAuthority('dept:tree:data')")
-    @PostMapping
-    public Result<MemberRechargeVO> create(
-            @Max(value = 10000, message = "充值金额单次最多允许充值10000元") @Min(value = 1, message = "充值金额单次最少充值金额为1元")
-                    BigDecimal price) {
-        MemberRechargeVO recharge = this.memberRechargeApi.recharge(price);
-        return Result.success(recharge);
-    }
+	@Operation(summary = "创建余额充值订单", description = "创建余额充值订单")
+	@RequestLogger
+	@PreAuthorize("hasAuthority('dept:tree:data')")
+	@PostMapping
+	public Result<MemberRechargeVO> create(@Max(value = 10000, message = "充值金额单次最多允许充值10000元")
+										   @Min(value = 1, message = "充值金额单次最少充值金额为1元")
+										   BigDecimal price) {
+		MemberRechargeVO recharge = this.memberRechargeApi.recharge(price);
+		return Result.success(recharge);
+	}
 }
