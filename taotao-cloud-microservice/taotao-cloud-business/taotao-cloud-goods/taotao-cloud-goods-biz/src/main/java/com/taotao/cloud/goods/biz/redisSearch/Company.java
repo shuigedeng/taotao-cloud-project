adb1f715cb8e57b0162cdf7ac24007cbc6410824
shuigedeng <dengtao>
2023-08-08 15:40:34 +0800

    [ taotao-cloud-project-2012 ] update[ version to 2023.09]

---
+++ taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-goods/taotao-cloud-goods-biz/src/main/java/com/taotao/cloud/goods/biz/redisSearch/Company.java
@@ -0,0 +1,38 @@
+package com.taotao.cloud.goods.biz.redisSearch;
+
+import com.redis.om.spring.annotations.Document;
+import com.redis.om.spring.annotations.Indexed;
+import com.redis.om.spring.annotations.Searchable;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.geo.Point;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor(staticName = "of")
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Document
+public class Company {
+  //@Id：声明主键，RedisOM将会通过全类名:ID这样的键来存储数据；
+    @Id
+    private String id;
+    //@Searchable：声明可以搜索的索引，通常用在文本类型上。
+    @Searchable
+    private String name;
+    //@Indexed：声明索引，通常用在非文本类型上；
+    @Indexed
+    private Point location;
+    @Indexed
+    private Set<String> tags = new HashSet<>();
+    @Indexed
+    private Integer numberOfEmployees;
+    @Indexed
+    private Integer yearFounded;
+    private String url;
+    private boolean publiclyListed;
+
+    // ...
+}
