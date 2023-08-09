747a4fab36bcddb86bd2b37ecf2220ddee9d2027
shuigedeng <dengtao>
2023-08-09 16:59:09 +0800

    [ taotao-cloud-project-2012 ] update[ version to 2023.09]

---
+++ taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-sys/taotao-cloud-sys-biz/src/main/java/com/taotao/cloud/sys/biz/mbg/AppDynamicSqlSupport.java
@@ -0,0 +1,74 @@
+package com.taotao.cloud.sys.biz.mbg;
+
+import jakarta.annotation.Generated;
+import java.sql.JDBCType;
+import java.time.LocalDateTime;
+import org.mybatis.dynamic.sql.AliasableSqlTable;
+import org.mybatis.dynamic.sql.SqlColumn;
+
+public final class AppDynamicSqlSupport {
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public static final App app = new App();
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public static final SqlColumn<Long> id = app.id;
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public static final SqlColumn<Long> createBy = app.createBy;
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public static final SqlColumn<LocalDateTime> createTime = app.createTime;
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public static final SqlColumn<Boolean> delFlag = app.delFlag;
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public static final SqlColumn<Long> updateBy = app.updateBy;
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public static final SqlColumn<LocalDateTime> updateTime = app.updateTime;
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public static final SqlColumn<Integer> version = app.version;
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public static final SqlColumn<String> code = app.code;
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public static final SqlColumn<String> icon = app.icon;
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public static final SqlColumn<String> name = app.name;
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public static final SqlColumn<Integer> sort = app.sort;
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public static final class App extends AliasableSqlTable<App> {
+        public final SqlColumn<Long> id = column("id", JDBCType.BIGINT);
+
+        public final SqlColumn<Long> createBy = column("create_by", JDBCType.BIGINT);
+
+        public final SqlColumn<LocalDateTime> createTime = column("create_time", JDBCType.TIMESTAMP);
+
+        public final SqlColumn<Boolean> delFlag = column("del_flag", JDBCType.BIT);
+
+        public final SqlColumn<Long> updateBy = column("update_by", JDBCType.BIGINT);
+
+        public final SqlColumn<LocalDateTime> updateTime = column("update_time", JDBCType.TIMESTAMP);
+
+        public final SqlColumn<Integer> version = column("version", JDBCType.INTEGER);
+
+        public final SqlColumn<String> code = column("code", JDBCType.VARCHAR);
+
+        public final SqlColumn<String> icon = column("icon", JDBCType.VARCHAR);
+
+        public final SqlColumn<String> name = column("`name`", JDBCType.VARCHAR);
+
+        public final SqlColumn<Integer> sort = column("sort", JDBCType.INTEGER);
+
+        public App() {
+            super("tt_app", App::new);
+        }
+    }
+}
