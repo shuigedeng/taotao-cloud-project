5ce329929967896b10fb7cc8eb9a5ff2b56f0c49
shuigedeng <dengtao>
2023-08-09 16:59:09 +0800

    [ taotao-cloud-project-2012 ] update[ version to 2023.09]

---
+++ taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-sys/taotao-cloud-sys-biz/src/main/java/com/taotao/cloud/sys/biz/mbg/AppMapper.java
@@ -0,0 +1,226 @@
+package com.taotao.cloud.sys.biz.mbg;
+
+import static com.taotao.cloud.sys.biz.mbg.AppDynamicSqlSupport.*;
+import static org.mybatis.dynamic.sql.SqlBuilder.isEqualTo;
+
+import com.taotao.cloud.sys.biz.mbg.App;
+import jakarta.annotation.Generated;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import org.apache.ibatis.annotations.InsertProvider;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.ResultMap;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.SelectProvider;
+import org.apache.ibatis.type.JdbcType;
+import org.mybatis.dynamic.sql.BasicColumn;
+import org.mybatis.dynamic.sql.delete.DeleteDSLCompleter;
+import org.mybatis.dynamic.sql.insert.render.InsertStatementProvider;
+import org.mybatis.dynamic.sql.select.CountDSLCompleter;
+import org.mybatis.dynamic.sql.select.SelectDSLCompleter;
+import org.mybatis.dynamic.sql.select.render.SelectStatementProvider;
+import org.mybatis.dynamic.sql.update.UpdateDSL;
+import org.mybatis.dynamic.sql.update.UpdateDSLCompleter;
+import org.mybatis.dynamic.sql.update.UpdateModel;
+import org.mybatis.dynamic.sql.util.SqlProviderAdapter;
+import org.mybatis.dynamic.sql.util.mybatis3.CommonCountMapper;
+import org.mybatis.dynamic.sql.util.mybatis3.CommonDeleteMapper;
+import org.mybatis.dynamic.sql.util.mybatis3.CommonSelectMapper;
+import org.mybatis.dynamic.sql.util.mybatis3.CommonUpdateMapper;
+import org.mybatis.dynamic.sql.util.mybatis3.MyBatis3Utils;
+
+@Mapper
+public interface AppMapper extends CommonSelectMapper, CommonCountMapper, CommonDeleteMapper, CommonUpdateMapper {
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    BasicColumn[] selectList = BasicColumn.columnList(id, createBy, createTime, delFlag, updateBy, updateTime, version, code, icon, name, sort);
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    @InsertProvider(type=SqlProviderAdapter.class, method="insert")
+    @Options(useGeneratedKeys=true,keyProperty="row.id")
+    int insert(InsertStatementProvider<App> insertStatement);
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    @InsertProvider(type=SqlProviderAdapter.class, method="insertMultipleWithGeneratedKeys")
+    @Options(useGeneratedKeys=true,keyProperty="records.id")
+    int insertMultiple(@Param("insertStatement") String insertStatement, @Param("records") List<App> records);
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    @SelectProvider(type=SqlProviderAdapter.class, method="select")
+    @Results(id="AppResult", value = {
+        @Result(column="id", property="id", jdbcType=JdbcType.BIGINT, id=true),
+        @Result(column="create_by", property="createBy", jdbcType=JdbcType.BIGINT),
+        @Result(column="create_time", property="createTime", jdbcType=JdbcType.TIMESTAMP),
+        @Result(column="del_flag", property="delFlag", jdbcType=JdbcType.BIT),
+        @Result(column="update_by", property="updateBy", jdbcType=JdbcType.BIGINT),
+        @Result(column="update_time", property="updateTime", jdbcType=JdbcType.TIMESTAMP),
+        @Result(column="version", property="version", jdbcType=JdbcType.INTEGER),
+        @Result(column="code", property="code", jdbcType=JdbcType.VARCHAR),
+        @Result(column="icon", property="icon", jdbcType=JdbcType.VARCHAR),
+        @Result(column="name", property="name", jdbcType=JdbcType.VARCHAR),
+        @Result(column="sort", property="sort", jdbcType=JdbcType.INTEGER)
+    })
+    List<App> selectMany(SelectStatementProvider selectStatement);
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    @SelectProvider(type=SqlProviderAdapter.class, method="select")
+    @ResultMap("AppResult")
+    Optional<App> selectOne(SelectStatementProvider selectStatement);
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    default long count(CountDSLCompleter completer) {
+        return MyBatis3Utils.countFrom(this::count, app, completer);
+    }
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    default int delete(DeleteDSLCompleter completer) {
+        return MyBatis3Utils.deleteFrom(this::delete, app, completer);
+    }
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    default int deleteByPrimaryKey(Long id_) {
+        return delete(c -> 
+            c.where(id, isEqualTo(id_))
+        );
+    }
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    default int insert(App row) {
+        return MyBatis3Utils.insert(this::insert, row, app, c ->
+            c.map(createBy).toProperty("createBy")
+            .map(createTime).toProperty("createTime")
+            .map(delFlag).toProperty("delFlag")
+            .map(updateBy).toProperty("updateBy")
+            .map(updateTime).toProperty("updateTime")
+            .map(version).toProperty("version")
+            .map(code).toProperty("code")
+            .map(icon).toProperty("icon")
+            .map(name).toProperty("name")
+            .map(sort).toProperty("sort")
+        );
+    }
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    default int insertMultiple(Collection<App> records) {
+        return MyBatis3Utils.insertMultipleWithGeneratedKeys(this::insertMultiple, records, app, c ->
+            c.map(createBy).toProperty("createBy")
+            .map(createTime).toProperty("createTime")
+            .map(delFlag).toProperty("delFlag")
+            .map(updateBy).toProperty("updateBy")
+            .map(updateTime).toProperty("updateTime")
+            .map(version).toProperty("version")
+            .map(code).toProperty("code")
+            .map(icon).toProperty("icon")
+            .map(name).toProperty("name")
+            .map(sort).toProperty("sort")
+        );
+    }
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    default int insertSelective(App row) {
+        return MyBatis3Utils.insert(this::insert, row, app, c ->
+            c.map(createBy).toPropertyWhenPresent("createBy", row::getCreateBy)
+            .map(createTime).toPropertyWhenPresent("createTime", row::getCreateTime)
+            .map(delFlag).toPropertyWhenPresent("delFlag", row::getDelFlag)
+            .map(updateBy).toPropertyWhenPresent("updateBy", row::getUpdateBy)
+            .map(updateTime).toPropertyWhenPresent("updateTime", row::getUpdateTime)
+            .map(version).toPropertyWhenPresent("version", row::getVersion)
+            .map(code).toPropertyWhenPresent("code", row::getCode)
+            .map(icon).toPropertyWhenPresent("icon", row::getIcon)
+            .map(name).toPropertyWhenPresent("name", row::getName)
+            .map(sort).toPropertyWhenPresent("sort", row::getSort)
+        );
+    }
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    default Optional<App> selectOne(SelectDSLCompleter completer) {
+        return MyBatis3Utils.selectOne(this::selectOne, selectList, app, completer);
+    }
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    default List<App> select(SelectDSLCompleter completer) {
+        return MyBatis3Utils.selectList(this::selectMany, selectList, app, completer);
+    }
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    default List<App> selectDistinct(SelectDSLCompleter completer) {
+        return MyBatis3Utils.selectDistinct(this::selectMany, selectList, app, completer);
+    }
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    default Optional<App> selectByPrimaryKey(Long id_) {
+        return selectOne(c ->
+            c.where(id, isEqualTo(id_))
+        );
+    }
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    default int update(UpdateDSLCompleter completer) {
+        return MyBatis3Utils.update(this::update, app, completer);
+    }
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    static UpdateDSL<UpdateModel> updateAllColumns(App row, UpdateDSL<UpdateModel> dsl) {
+        return dsl.set(createBy).equalTo(row::getCreateBy)
+                .set(createTime).equalTo(row::getCreateTime)
+                .set(delFlag).equalTo(row::getDelFlag)
+                .set(updateBy).equalTo(row::getUpdateBy)
+                .set(updateTime).equalTo(row::getUpdateTime)
+                .set(version).equalTo(row::getVersion)
+                .set(code).equalTo(row::getCode)
+                .set(icon).equalTo(row::getIcon)
+                .set(name).equalTo(row::getName)
+                .set(sort).equalTo(row::getSort);
+    }
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    static UpdateDSL<UpdateModel> updateSelectiveColumns(App row, UpdateDSL<UpdateModel> dsl) {
+        return dsl.set(createBy).equalToWhenPresent(row::getCreateBy)
+                .set(createTime).equalToWhenPresent(row::getCreateTime)
+                .set(delFlag).equalToWhenPresent(row::getDelFlag)
+                .set(updateBy).equalToWhenPresent(row::getUpdateBy)
+                .set(updateTime).equalToWhenPresent(row::getUpdateTime)
+                .set(version).equalToWhenPresent(row::getVersion)
+                .set(code).equalToWhenPresent(row::getCode)
+                .set(icon).equalToWhenPresent(row::getIcon)
+                .set(name).equalToWhenPresent(row::getName)
+                .set(sort).equalToWhenPresent(row::getSort);
+    }
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    default int updateByPrimaryKey(App row) {
+        return update(c ->
+            c.set(createBy).equalTo(row::getCreateBy)
+            .set(createTime).equalTo(row::getCreateTime)
+            .set(delFlag).equalTo(row::getDelFlag)
+            .set(updateBy).equalTo(row::getUpdateBy)
+            .set(updateTime).equalTo(row::getUpdateTime)
+            .set(version).equalTo(row::getVersion)
+            .set(code).equalTo(row::getCode)
+            .set(icon).equalTo(row::getIcon)
+            .set(name).equalTo(row::getName)
+            .set(sort).equalTo(row::getSort)
+            .where(id, isEqualTo(row::getId))
+        );
+    }
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    default int updateByPrimaryKeySelective(App row) {
+        return update(c ->
+            c.set(createBy).equalToWhenPresent(row::getCreateBy)
+            .set(createTime).equalToWhenPresent(row::getCreateTime)
+            .set(delFlag).equalToWhenPresent(row::getDelFlag)
+            .set(updateBy).equalToWhenPresent(row::getUpdateBy)
+            .set(updateTime).equalToWhenPresent(row::getUpdateTime)
+            .set(version).equalToWhenPresent(row::getVersion)
+            .set(code).equalToWhenPresent(row::getCode)
+            .set(icon).equalToWhenPresent(row::getIcon)
+            .set(name).equalToWhenPresent(row::getName)
+            .set(sort).equalToWhenPresent(row::getSort)
+            .where(id, isEqualTo(row::getId))
+        );
+    }
+}
