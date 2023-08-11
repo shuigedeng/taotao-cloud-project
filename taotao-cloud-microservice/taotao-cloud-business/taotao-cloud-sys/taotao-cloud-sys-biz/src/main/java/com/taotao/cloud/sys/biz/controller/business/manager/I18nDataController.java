adfaf7d5946b97e1bb678614030fabd31305aba3
shuigedeng <dengtao>
2023-08-11 17:58:16 +0800

    build dockerfile taotao-cloud-sys

--- taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-sys/taotao-cloud-sys-biz/src/main/java/com/taotao/cloud/sys/biz/controller/business/manager/I18nDataController.java
+++ taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-sys/taotao-cloud-sys-biz/src/main/java/com/taotao/cloud/sys/biz/controller/business/manager/I18nDataController.java
@@ -38,110 +38,110 @@ public class I18nDataController {
 
 	private final I18nDataService i18nDataService;
 
-	@GetMapping("/page")
-	@PreAuthorize("@per.hasPermission('i18n:i18n-data:read')")
-	@Operation(summary = "分页查询", description = "分页查询")
-	public Result<PageResult<I18nDataPageVO>> getI18nDataPage(@Validated PageQuery pageParam, I18nDataQO i18nDataQO) {
-		IPage<I18nDataPageVO> page = i18nDataService.queryPage(pageParam, i18nDataQO);
-		return Result.success(PageResult.convertMybatisPage(page, I18nDataPageVO.class));
-	}
-
-	@GetMapping("/list")
-	@PreAuthorize("@per.hasPermission('i18n:i18n-data:read')")
-	@Operation(summary = "查询指定国际化标识的所有数据", description = "查询指定国际化标识的所有数据")
-	public Result<List<I18nData>> listByCode(@RequestParam("code") String code) {
-		return Result.success(i18nDataService.listByCode(code));
-	}
-
-	@PostMapping
-	@PreAuthorize("@per.hasPermission('i18n:i18n-data:add')")
-	@Operation(summary = "新增国际化信息", description = "新增国际化信息")
-	public Result<Boolean> save(@Valid @RequestBody I18nDataCreateDTO i18nDataCreateDTO) {
-		// 转换为实体类列表
-		List<I18nData> list = new ArrayList<>();
-		List<I18nDataCreateDTO.LanguageText> languageTexts = i18nDataCreateDTO.getLanguageTexts();
-		for (I18nDataCreateDTO.LanguageText languageText : languageTexts) {
-			I18nData i18nData = new I18nData();
-			i18nData.setCode(i18nDataCreateDTO.getCode());
-			i18nData.setRemarks(i18nDataCreateDTO.getRemarks());
-			i18nData.setLanguageTag(languageText.getLanguageTag());
-			i18nData.setMessage(languageText.getMessage());
-			list.add(i18nData);
-		}
-//		return i18nDataService.saveBatch(list) ? R.ok() : R.failed(BaseResultCode.UPDATE_DATABASE_ERROR, "新增国际化信息失败");
-		return Result.success(true);
-	}
-
-	@PutMapping
-	@PreAuthorize("@per.hasPermission('i18n:i18n-data:edit')")
-	@Operation(summary = "修改国际化信息", description = "修改国际化信息")
-	public Result<Boolean> updateById(@RequestBody I18nDataDTO i18nDataDTO) {
-//		return i18nDataService.updateByCodeAndLanguageTag(i18nDataDTO) ? R.ok()
-//			: R.failed(BaseResultCode.UPDATE_DATABASE_ERROR, "修改国际化信息失败");
-		return Result.success(true);
-	}
-
-	@DeleteMapping
-	@PreAuthorize("@per.hasPermission('i18n:i18n-data:del')")
-	@Operation(summary = "通过id删除国际化信息", description = "通过id删除国际化信息")
-	public Result<Boolean> removeById(@RequestParam("code") String code, @RequestParam("languageTag") String languageTag) {
-//		return i18nDataService.removeByCodeAndLanguageTag(code, languageTag) ? R.ok()
-//			: R.failed(BaseResultCode.UPDATE_DATABASE_ERROR, "通过id删除国际化信息失败");
-		return Result.success(true);
-	}
-
-	@PostMapping("/import")
-	@PreAuthorize("@per.hasPermission('i18n:i18n-data:import')")
-	@Operation(summary = "导入国际化信息", description = "导入国际化信息")
-	public Result<Boolean> importI18nData(
-//		@RequestParam("importMode") ImportModeEnum importModeEnum,
-		@RequestExcel List<I18nDataExcelVO> excelVos
-	) {
-
-		if (CollUtil.isEmpty(excelVos)) {
-			return Result.success(true);
-		}
-
-		// 转换结构
-		List<I18nData> list = excelVos.stream()
-			.map(I18nDataConverter.INSTANCE::excelVoToPo)
-			.toList();
-
-		// 跳过已有数据，返回已有数据列表
-//		if (importModeEnum == ImportModeEnum.SKIP_EXISTING) {
-//			List<I18nData> existsList = i18nDataService.saveWhenNotExist(list);
+//	@GetMapping("/page")
+//	@PreAuthorize("@per.hasPermission('i18n:i18n-data:read')")
+//	@Operation(summary = "分页查询", description = "分页查询")
+//	public Result<PageResult<I18nDataPageVO>> getI18nDataPage(@Validated PageQuery pageParam, I18nDataQO i18nDataQO) {
+//		IPage<I18nDataPageVO> page = i18nDataService.queryPage(pageParam, i18nDataQO);
+//		return Result.success(PageResult.convertMybatisPage(page, I18nDataPageVO.class));
+//	}
+//
+//	@GetMapping("/list")
+//	@PreAuthorize("@per.hasPermission('i18n:i18n-data:read')")
+//	@Operation(summary = "查询指定国际化标识的所有数据", description = "查询指定国际化标识的所有数据")
+//	public Result<List<I18nData>> listByCode(@RequestParam("code") String code) {
+//		return Result.success(i18nDataService.listByCode(code));
+//	}
+//
+//	@PostMapping
+//	@PreAuthorize("@per.hasPermission('i18n:i18n-data:add')")
+//	@Operation(summary = "新增国际化信息", description = "新增国际化信息")
+//	public Result<Boolean> save(@Valid @RequestBody I18nDataCreateDTO i18nDataCreateDTO) {
+//		// 转换为实体类列表
+//		List<I18nData> list = new ArrayList<>();
+//		List<I18nDataCreateDTO.LanguageText> languageTexts = i18nDataCreateDTO.getLanguageTexts();
+//		for (I18nDataCreateDTO.LanguageText languageText : languageTexts) {
+//			I18nData i18nData = new I18nData();
+//			i18nData.setCode(i18nDataCreateDTO.getCode());
+//			i18nData.setRemarks(i18nDataCreateDTO.getRemarks());
+//			i18nData.setLanguageTag(languageText.getLanguageTag());
+//			i18nData.setMessage(languageText.getMessage());
+//			list.add(i18nData);
+//		}
+////		return i18nDataService.saveBatch(list) ? R.ok() : R.failed(BaseResultCode.UPDATE_DATABASE_ERROR, "新增国际化信息失败");
+//		return Result.success(true);
+//	}
+//
+//	@PutMapping
+//	@PreAuthorize("@per.hasPermission('i18n:i18n-data:edit')")
+//	@Operation(summary = "修改国际化信息", description = "修改国际化信息")
+//	public Result<Boolean> updateById(@RequestBody I18nDataDTO i18nDataDTO) {
+////		return i18nDataService.updateByCodeAndLanguageTag(i18nDataDTO) ? R.ok()
+////			: R.failed(BaseResultCode.UPDATE_DATABASE_ERROR, "修改国际化信息失败");
+//		return Result.success(true);
+//	}
+//
+//	@DeleteMapping
+//	@PreAuthorize("@per.hasPermission('i18n:i18n-data:del')")
+//	@Operation(summary = "通过id删除国际化信息", description = "通过id删除国际化信息")
+//	public Result<Boolean> removeById(@RequestParam("code") String code, @RequestParam("languageTag") String languageTag) {
+////		return i18nDataService.removeByCodeAndLanguageTag(code, languageTag) ? R.ok()
+////			: R.failed(BaseResultCode.UPDATE_DATABASE_ERROR, "通过id删除国际化信息失败");
+//		return Result.success(true);
+//	}
+//
+//	@PostMapping("/import")
+//	@PreAuthorize("@per.hasPermission('i18n:i18n-data:import')")
+//	@Operation(summary = "导入国际化信息", description = "导入国际化信息")
+//	public Result<Boolean> importI18nData(
+////		@RequestParam("importMode") ImportModeEnum importModeEnum,
+//		@RequestExcel List<I18nDataExcelVO> excelVos
+//	) {
+//
+//		if (CollUtil.isEmpty(excelVos)) {
 //			return Result.success(true);
 //		}
 //
-//		// 覆盖已有数据
-//		if (importModeEnum == ImportModeEnum.OVERWRITE_EXISTING) {
-//			i18nDataService.saveOrUpdate(list);
+//		// 转换结构
+//		List<I18nData> list = excelVos.stream()
+//			.map(I18nDataConverter.INSTANCE::excelVoToPo)
+//			.toList();
+//
+//		// 跳过已有数据，返回已有数据列表
+////		if (importModeEnum == ImportModeEnum.SKIP_EXISTING) {
+////			List<I18nData> existsList = i18nDataService.saveWhenNotExist(list);
+////			return Result.success(true);
+////		}
+////
+////		// 覆盖已有数据
+////		if (importModeEnum == ImportModeEnum.OVERWRITE_EXISTING) {
+////			i18nDataService.saveOrUpdate(list);
+////		}
+//
+//		return Result.success(true);
+//	}
+//
+//	@ResponseExcel(name = "国际化信息", i18nHeader = true)
+//	@GetMapping("/export")
+//	@PreAuthorize("@per.hasPermission('i18n:i18n-data:export')")
+//	@Operation(summary = "导出国际化信息", description = "导出国际化信息")
+//	public List<I18nDataExcelVO> exportI18nData(I18nDataQO i18nDataQO) {
+//		List<I18nData> list = i18nDataService.queryList(i18nDataQO);
+//		if (CollUtil.isEmpty(list)) {
+//			return new ArrayList<>();
 //		}
-
-		return Result.success(true);
-	}
-
-	@ResponseExcel(name = "国际化信息", i18nHeader = true)
-	@GetMapping("/export")
-	@PreAuthorize("@per.hasPermission('i18n:i18n-data:export')")
-	@Operation(summary = "导出国际化信息", description = "导出国际化信息")
-	public List<I18nDataExcelVO> exportI18nData(I18nDataQO i18nDataQO) {
-		List<I18nData> list = i18nDataService.queryList(i18nDataQO);
-		if (CollUtil.isEmpty(list)) {
-			return new ArrayList<>();
-		}
-		// 转换为 excel vo 对象
-		return list.stream().map(I18nDataConverter.INSTANCE::poToExcelVo).toList();
-	}
-
-	@ResponseExcel(name = "国际化信息模板", i18nHeader = true)
-	@GetMapping("/excel-template")
-	@PreAuthorize("@per.hasPermission('i18n:i18n-data:import')")
-	@Operation(summary = "国际化信息 Excel 模板", description = "国际化信息 Excel 模板")
-	public List<I18nDataExcelVO> excelTemplate() {
-		List<I18nDataExcelVO> list = new ArrayList<>();
-		list.add(new I18nDataExcelVO());
-		return list;
-	}
+//		// 转换为 excel vo 对象
+//		return list.stream().map(I18nDataConverter.INSTANCE::poToExcelVo).toList();
+//	}
+//
+//	@ResponseExcel(name = "国际化信息模板", i18nHeader = true)
+//	@GetMapping("/excel-template")
+//	@PreAuthorize("@per.hasPermission('i18n:i18n-data:import')")
+//	@Operation(summary = "国际化信息 Excel 模板", description = "国际化信息 Excel 模板")
+//	public List<I18nDataExcelVO> excelTemplate() {
+//		List<I18nDataExcelVO> list = new ArrayList<>();
+//		list.add(new I18nDataExcelVO());
+//		return list;
+//	}
 
 }
--- taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-sys/taotao-cloud-sys-biz/src/main/java/com/taotao/cloud/sys/biz/controller/business/manager/ManagerUserController.java
+++ taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-sys/taotao-cloud-sys-biz/src/main/java/com/taotao/cloud/sys/biz/controller/business/manager/ManagerUserController.java
@@ -24,6 +24,7 @@ import com.taotao.cloud.common.utils.bean.BeanUtils;
 import com.taotao.cloud.common.utils.common.SecurityUtils;
 import com.taotao.cloud.security.springsecurity.annotation.NotAuth;
 import com.taotao.cloud.sys.api.model.dto.user.RestPasswordUserDTO;
+import com.taotao.cloud.sys.api.model.dto.user.UpdateUserRolesDTO;
 import com.taotao.cloud.sys.api.model.dto.user.UserSaveDTO;
 import com.taotao.cloud.sys.api.model.dto.user.UserUpdateDTO;
 import com.taotao.cloud.sys.api.model.vo.user.UserQueryVO;
@@ -41,6 +42,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -61,109 +63,120 @@ import java.util.Set;
 @RequestMapping("/sys/manager/user")
 @Tag(name = "管理端-用户管理API", description = "管理端-用户管理API")
 public class ManagerUserController
-        extends BaseSuperController<IUserService, User, Long, BaseQuery, UserSaveDTO, UserUpdateDTO, UserQueryVO> {
+	extends BaseSuperController<IUserService, User, Long, BaseQuery, UserSaveDTO, UserUpdateDTO, UserQueryVO> {
 
-    @Operation(summary = "根据手机号码查询用户是否存在",
-            description = "根据手机号码查询用户是否存在",
-            parameters = {
-                    @Parameter(name = "phone", description = "手机号码", required = true, example = "15730445331", in = ParameterIn.PATH)
-            })
-    @RequestLogger
-    @PreAuthorize("hasAuthority('sys:user:exists:phone')")
-    @GetMapping("/exists/phone/{phone}")
-    public Result<Boolean> existsByPhone(@NotBlank(message = "手机号码不能为空") @PathVariable(name = "phone") String phone) {
-        return success(service().existsByPhone(phone));
-    }
+	@Operation(summary = "根据手机号码查询用户是否存在",
+		description = "根据手机号码查询用户是否存在",
+		parameters = {
+			@Parameter(name = "phone", description = "手机号码", required = true, example = "15730445331", in = ParameterIn.PATH)
+		})
+	@RequestLogger
+	@PreAuthorize("hasAuthority('sys:user:exists:phone')")
+	@GetMapping("/exists/phone/{phone}")
+	public Result<Boolean> existsByPhone(@NotBlank(message = "手机号码不能为空") @PathVariable(name = "phone") String phone) {
+		return success(service().existsByPhone(phone));
+	}
 
-    @Operation(summary = "根据用户id查询用户是否存在",
-            description = "根据用户id查询用户是否存在",
-            parameters = {
-                    @Parameter(name = "userId", description = "用户id", required = true, example = "123", in = ParameterIn.PATH)
-            })
-    @RequestLogger
-    @PreAuthorize("hasAuthority('sys:user:exists:id')")
-    @GetMapping("/exists/id/{userId}")
-    public Result<Boolean> existsByUserId(@NotNull(message = "用户id不能为空") @PathVariable(name = "userId") Long userId) {
-        return success(service().existsById(userId));
-    }
+	@Operation(summary = "根据用户id查询用户是否存在",
+		description = "根据用户id查询用户是否存在",
+		parameters = {
+			@Parameter(name = "userId", description = "用户id", required = true, example = "123", in = ParameterIn.PATH)
+		})
+	@RequestLogger
+	@PreAuthorize("hasAuthority('sys:user:exists:id')")
+	@GetMapping("/exists/id/{userId}")
+	public Result<Boolean> existsByUserId(@NotNull(message = "用户id不能为空") @PathVariable(name = "userId") Long userId) {
+		return success(service().existsById(userId));
+	}
 
-    @Operation(summary = "重置密码",
-            description = "后台页面-用户信息页面-重置密码",
-            parameters = {
-                    @Parameter(name = "userId", description = "用户id", required = true, example = "123", in = ParameterIn.PATH)
-            },
-            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
-                    description = "重置密码DTO",
-                    required = true,
-                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = RestPasswordUserDTO.class))}
-            ))
-    @RequestLogger
-    @PreAuthorize("hasAuthority('sys:user:rest:password')")
-    @PostMapping("/rest/password/{userId}")
-    public Result<Boolean> restPass(@NotNull(message = "用户id不能为空") @PathVariable(name = "userId") Long userId,
-                                    @Validated @RequestBody RestPasswordUserDTO restPasswordDTO) {
-        return success(service().restPass(userId, restPasswordDTO));
-    }
+	@Operation(summary = "重置密码",
+		description = "后台页面-用户信息页面-重置密码",
+		parameters = {
+			@Parameter(name = "userId", description = "用户id", required = true, example = "123", in = ParameterIn.PATH)
+		},
+		requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+			description = "重置密码DTO",
+			required = true,
+			content = {@Content(mediaType = "application/json", schema = @Schema(implementation = RestPasswordUserDTO.class))}
+		))
+	@RequestLogger
+	@PreAuthorize("hasAuthority('sys:user:rest:password')")
+	@PostMapping("/rest/password/{userId}")
+	public Result<Boolean> restPass(@NotNull(message = "用户id不能为空") @PathVariable(name = "userId") Long userId,
+									@Validated @RequestBody RestPasswordUserDTO restPasswordDTO) {
+		return success(service().restPass(userId, restPasswordDTO));
+	}
 
-    @Operation(summary = "获取当前登录人信息", description = "获取当前登录人信息")
-    @RequestLogger
-    @PreAuthorize("hasAuthority('sys:user:info:current')")
-    @GetMapping("/current")
-    public Result<UserQueryVO> getCurrentUser() {
-        SecurityUser securityUser = SecurityUtils.getCurrentUser();
-        if (Objects.isNull(securityUser)) {
-            throw new BusinessException("用户未登录");
-        }
-        Long userId = securityUser.getUserId();
-        User sysUser = service().getById(userId);
-        return success(UserConvert.INSTANCE.convert(sysUser));
-    }
+	@Operation(summary = "获取当前登录人信息", description = "获取当前登录人信息")
+	@RequestLogger
+	@PreAuthorize("hasAuthority('sys:user:info:current')")
+	@GetMapping("/current")
+	public Result<UserQueryVO> getCurrentUser() {
+		SecurityUser securityUser = SecurityUtils.getCurrentUser();
+		if (Objects.isNull(securityUser)) {
+			throw new BusinessException("用户未登录");
+		}
+		Long userId = securityUser.getUserId();
+		User sysUser = service().getById(userId);
+		return success(UserConvert.INSTANCE.convert(sysUser));
+	}
 
-    @Operation(summary = "根据用户id更新角色信息(用户分配角色)",
-            description = "后台页面-用户信息页面-根据用户id更新角色信息(用户分配角色)",
-            parameters = {
-                    @Parameter(name = "userId", description = "用户id", required = true, example = "123", in = ParameterIn.PATH)
-            },
-            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
-                    description = "角色id列表",
-                    required = true,
-                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Set.class))}
-            ))
-    @RequestLogger
-    @PreAuthorize("hasAuthority('sys:user:role')")
-    @PutMapping("/roles/{userId}")
-    public Result<Boolean> updateUserRoles(@NotNull(message = "用户id不能为空") @PathVariable(name = "userId") Long userId,
-                                           @Validated @NotEmpty(message = "角色id列表不能为空") @RequestBody Set<Long> roleIds) {
-        return success(service().updateUserRoles(userId, roleIds));
-    }
+	@Operation(summary = "根据用户id更新角色信息(用户分配角色)",
+		description = "后台页面-用户信息页面-根据用户id更新角色信息(用户分配角色)",
+		parameters = {
+			@Parameter(name = "userId", description = "用户id", required = true, example = "123", in = ParameterIn.PATH)
+		},
+		requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+			description = "用户更新角色信息DTO",
+			required = true,
+			content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = UpdateUserRolesDTO.class))}
+		))
+	@RequestLogger
+	@PreAuthorize("hasAuthority('sys:user:role')")
+	@PutMapping("/roles/{userId}")
+	@NotAuth
+	public Result<Boolean> updateUserRoles(@NotNull(message = "用户id不能为空") @PathVariable(name = "userId") Long userId,
+										   @Validated @RequestBody UpdateUserRolesDTO updateUserRolesDTO) {
+		return success(service().updateUserRoles(userId, updateUserRolesDTO.getRoleIds()));
+	}
 
-    @PostMapping("/user/test/save")
-    @NotAuth
-    public Result<Boolean> testSave(@Parameter(description = "新增DTO", required = true)
-                                    @RequestBody @Validated UserSaveDTO saveDTO) {
-        User user = new User();
-        BeanUtils.copy(saveDTO, user);
-        user.setAccount("sdfasfd");
-        user.setNickname("sdfasfd");
-        user.setUsername("sdfasfd");
-        user.setPassword("xxx");
-        user.setMobile("123455");
-        user.setPhone("123455");
-        user.setSex(0);
-        user.setEmail("sdfasdf");
-        user.setBirthday("sdfasdf");
-        // user.setDeptId(1L);
-        // user.setJobId(2L);
-        user.setStatus(1);
-        user.setTenantId("sdfasdf");
-        service().im().insert(user);
-        return Result.success(true);
-    }
+	@PostMapping("/user/test/save")
+	@Operation(summary = "test",
+		description = "test",
+		parameters = {
+			@Parameter(name = "userId", description = "用户id", required = true, example = "123")
+		},
+		requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+			description = "新增DTO",
+			required = true,
+			content = {@Content(mediaType = "application/json", schema = @Schema(implementation = UserSaveDTO.class))}
+		))
+	@NotAuth
+	public Result<Boolean> testSave(@NotNull(message = "用户id不能为空") @RequestParam(name = "userId") Long userId,
+									@Validated @RequestBody UserSaveDTO saveDTO) {
+		User user = new User();
+		BeanUtils.copy(saveDTO, user);
+		user.setAccount("sdfasfd");
+		user.setNickname("sdfasfd");
+		user.setUsername("sdfasfd");
+		user.setPassword("xxx");
+		user.setMobile("123455");
+		user.setPhone("123455");
+		user.setSex(0);
+		user.setEmail("sdfasdf");
+		user.setBirthday("sdfasdf");
+		// user.setDeptId(1L);
+		// user.setJobId(2L);
+		user.setStatus(1);
+		user.setTenantId("sdfasdf");
+		service().im().insert(user);
+		return Result.success(true);
+	}
 
-    @GetMapping("/user/test/get")
-    @NotAuth
-    public Result<List<User>> testGet() {
-        List<User> all = service().ir().findAll();
-        return Result.success(all);
-    }
+	@GetMapping("/user/test/get")
+	@NotAuth
+	public Result<List<User>> testGet() {
+		List<User> all = service().ir().findAll();
+		return Result.success(all);
+	}
 }
