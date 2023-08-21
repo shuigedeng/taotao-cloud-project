29c983a1241704b4e77e1195564ba254ff531bf2
shuigedeng <dengtao>
2023-08-21 17:46:44 +0800

    build dockerfile  taotao-cloud-file

--- dockerfile/taotao-cloud/healthcheck.sh
+++ dockerfile/taotao-cloud/healthcheck.sh
@@ -6,10 +6,13 @@
 
 function _main
 {
-  health_status_code=$(curl -s -o /dev/null -w "%{http_code}" -X GET "http://127.0.0.1:$1/actuator/health")
-  api_status_code=$(curl -s -o /dev/null -w "%{http_code}" -X GET "http://127.0.0.1:$1/v3/api-docs")
+  health_status_code=$(curl -s -o /dev/null -w "%{http_code}" -X GET "http://127.0.0.1:$APP_PORT/actuator/health")
+  api_status_code=$(curl -s -o /dev/null -w "%{http_code}" -X GET "http://127.0.0.1:$APP_PORT/v3/api-docs")
 
-  [[ $health_status_code -eq 200 && $health_status_code -eq 200 ]] && exit 1
+  echo $health_status_code
+  echo $api_status_code
+
+  [[ $health_status_code -eq 200 && $api_status_code -eq 200 ]] && exit 1
 
   exit 0
 }
