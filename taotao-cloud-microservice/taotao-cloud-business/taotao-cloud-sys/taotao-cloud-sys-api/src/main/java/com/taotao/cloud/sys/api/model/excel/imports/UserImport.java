f975e2a4d64f4c2c15a5e347730363fc49252588
dengtao <981376577@qq.com>
2023-12-19 15:38:19 +0800

    build: 修改版本

--- taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-sys/taotao-cloud-sys-api/src/main/java/com/taotao/cloud/sys/api/model/excel/imports/UserImport.java
+++ taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-sys/taotao-cloud-sys-api/src/main/java/com/taotao/cloud/sys/api/model/excel/imports/UserImport.java
@@ -17,34 +17,41 @@
 package com.taotao.cloud.sys.api.model.excel.imports;
 
 import cn.afterturn.easypoi.excel.annotation.Excel;
-import java.io.Serial;
-import java.io.Serializable;
 import lombok.Data;
 
+import java.io.Serial;
+import java.io.Serializable;
+
+/*
+ * UserImport
+ * @author shuigedeng
+ * @version 2023.12
+ * @since 2023-12-19 09:40:47
+ */
 @Data
 public class UserImport implements Serializable {
 
     @Serial
     private static final long serialVersionUID = -5298570444065758538L;
 
-    @Excel(name = "用户编号", orderNum = "0", height = 20, width = 30, isImportField = "true_st")
+    @Excel(name = "用户编号", orderNum = "0", width = 30, isImportField = "true_st")
     private Long id;
 
-    @Excel(name = "账号", orderNum = "1", height = 20, width = 30, isImportField = "true_st")
+    @Excel(name = "账号", orderNum = "1", width = 30, isImportField = "true_st")
     private String account;
 
-    @Excel(name = "昵称", orderNum = "2", height = 20, width = 30, isImportField = "true_st")
+    @Excel(name = "昵称", orderNum = "2", width = 30, isImportField = "true_st")
     private String name;
 
-    @Excel(name = "角色", orderNum = "3", height = 20, width = 30, isImportField = "true_st")
+    @Excel(name = "角色", orderNum = "3", width = 30, isImportField = "true_st")
     private String roleName;
 
-    @Excel(name = "部门", orderNum = "4", height = 20, width = 30, isImportField = "true_st")
+    @Excel(name = "部门", orderNum = "4", width = 30, isImportField = "true_st")
     private String departName;
 
-    @Excel(name = "电话", orderNum = "5", height = 20, width = 30, isImportField = "true_st")
+    @Excel(name = "电话", orderNum = "5", width = 30, isImportField = "true_st")
     private String telephone;
 
-    @Excel(name = "状态", orderNum = "6", height = 20, width = 30, isImportField = "true_st")
+    @Excel(name = "状态", orderNum = "6", width = 30, isImportField = "true_st")
     private String statusName;
 }
