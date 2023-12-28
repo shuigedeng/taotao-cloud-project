7149416f634b24ca80299029d345eaae8a9a3add
dengtao <981376577@qq.com>
2023-12-28 09:19:15 +0800

    build: 重构代码

---
+++ doc/scripts/taotao-cloud-monitor-start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+docker pull registry.cn-hangzhou.aliyuncs.com/taotao-cloud-project/taotao-cloud-monitor:2024.01
+
+docker tag registry.cn-hangzhou.aliyuncs.com/taotao-cloud-project/taotao-cloud-monitor:2024.01 taotao-cloud-monitor:latest
+
+docker stop monitor
+
+docker rm monitor
+
+docker run -d -p 33334:33334 --name monitor taotao-cloud-monitor:latest
+
+#删除未使用的镜像
+docker image prune -
