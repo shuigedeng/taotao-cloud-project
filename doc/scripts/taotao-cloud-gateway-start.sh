ddd7de11e82e9757edc595f564292deadb548adc
dengtao <981376577@qq.com>
2023-12-28 09:19:15 +0800

    build: 重构代码

---
+++ doc/scripts/taotao-cloud-gateway-start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+docker pull registry.cn-hangzhou.aliyuncs.com/taotao-cloud-project/taotao-cloud-gateway:2024.01
+
+docker tag registry.cn-hangzhou.aliyuncs.com/taotao-cloud-project/taotao-cloud-gateway:2024.01 taotao-cloud-gateway:latest
+
+docker stop gateway
+
+docker rm gateway
+
+docker run -d -p 33335:33335 --name gateway taotao-cloud-gateway:latest
+
+#删除未使用的镜像
+docker image prune -
