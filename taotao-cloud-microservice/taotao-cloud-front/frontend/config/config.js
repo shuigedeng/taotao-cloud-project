6fcac6f1561e4a26d0ef8784b5475a81c691f683
dengtao <981376577@qq.com>
2023-12-29 15:18:26 +0800

    build: 重构代码

--- taotao-cloud-microservice/taotao-cloud-front/frontend/config/config.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-fs.copyFile('src/main/webapp/index.html', 'build/www/index.html', (err) => {
-    if (err) {
-        throw err;
-    }
-    console.log('Copied index.html!');
-});
