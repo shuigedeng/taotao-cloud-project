ff9abfb7fde7663eb66b212e34dd177198280acb
shuigedeng <981376577@qq.com>
2024-02-23 18:59:21 +0800

    build: 重构代码

---
+++ taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-wechat/taotao-cloud-wechat-assembly/src/main/java/com/taotao/cloud/wechat/TaoTaoCloudWechatDDDApplication.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2020-2030, Shuigedeng (981376577@qq.com & https://blog.taotaocloud.top/).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.taotao.cloud.wechat;
+
+import com.taotao.cloud.common.utils.common.PropertyUtils;
+import com.taotao.cloud.core.startup.StartupSpringApplication;
+import com.taotao.cloud.web.annotation.TaoTaoCloudApplication;
+
+/**
+ * 系统管理中心
+ * <p>异常原因：
+ *
+ * 自从 JDK9 中引入了模块化功能后，再到 JDK17，对于包扫描和反射的权限控制更加的严格。常见的库比如（Spring）大量用到包扫描和反射，所以常出现此错误。
+ *
+ * 解决方案：
+ *
+ * 一个粗暴的解决办法是将没开放的 module 强制对外开放，即保持和 Java9 之前的版本一致。
+ *
+ * --add-exports 导出包，意味着其中的所有公共类型和成员都可以在编译和运行时访问。
+ * --add-opens 打开包，意味着其中的所有类型和成员（不仅是公共类型）都可以在运行时访问。
+ * 主要区别在于 --add-opens 允许 “深度反射”，即非公共成员的访问，才可以调用 setAccessible(true)
+ *
+ * <pre class="code">
+ * --add-opens java.base/java.lang=ALL-UNNAMED
+ * --add-opens java.base/java.lang.reflect=ALL-UNNAMED
+ * --add-opens java.base/java.lang.invoke=ALL-UNNAMED
+ * --add-opens java.base/java.util=ALL-UNNAMED
+ * --add-opens java.base/sun.net=ALL-UNNAMED
+ * --add-opens java.base/java.math=ALL-UNNAMED
+ * --add-opens java.base/sun.reflect.annotation=ALL-UNNAMED
+ * --add-opens java.base/sun.net=ALL-UNNAMED
+ * --add-opens java.desktop/sun.awt=ALL-UNNAMED
+ * --add-opens java.desktop/sun.font=ALL-UNNAMED
+ * --add-opens jdk.management/com.sun.management.internal=ALL-UNNAMED
+ * --add-exports java.desktop/sun.awt=ALL-UNNAMED
+ * --add-exports java.desktop/sun.font=ALL-UNNAMED
+ * </pre>
+ *
+ * @author shuigedeng
+ * @version 2022.03
+ * @since 2020/11/30 下午3:33
+ */
+@TaoTaoCloudApplication
+public class TaoTaoCloudWechatDDDApplication {
+
+	public static void main(String[] args) {
+		PropertyUtils.setDefaultProperty("taotao-cloud-ddd");
+		new StartupSpringApplication(TaoTaoCloudWechatDDDApplication.class)
+			.setDefaultBanner()
+			.run(args);
+	}
+}
---
+++ taotao-cloud-microservice/taotao-cloud-generator/src/main/java/com/taotao/cloud/generator/config/GenConfig.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2020-2030, Shuigedeng (981376577@qq.com & https://blog.taotaocloud.top/).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.taotao.cloud.generator.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * 代码生成相关配置
+ *
+ * @author ruoyi
+ */
+@Component
+@ConfigurationProperties(prefix = "gen")
+public class GenConfig {
+
+    /** 作者 */
+    public static String author;
+
+    /** 生成包路径 */
+    public static String packageName;
+
+    /** 自动去除表前缀，默认是false */
+    public static boolean autoRemovePre;
+
+    /** 表前缀(类名不会包含表前缀) */
+    public static String tablePrefix;
+
+    public static String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        GenConfig.author = author;
+    }
+
+    public static String getPackageName() {
+        return packageName;
+    }
+
+    public void setPackageName(String packageName) {
+        GenConfig.packageName = packageName;
+    }
+
+    public static boolean getAutoRemovePre() {
+        return autoRemovePre;
+    }
+
+    public void setAutoRemovePre(boolean autoRemovePre) {
+        GenConfig.autoRemovePre = autoRemovePre;
+    }
+
+    public static String getTablePrefix() {
+        return tablePrefix;
+    }
+
+    public void setTablePrefix(String tablePrefix) {
+        GenConfig.tablePrefix = tablePrefix;
+    }
+}
