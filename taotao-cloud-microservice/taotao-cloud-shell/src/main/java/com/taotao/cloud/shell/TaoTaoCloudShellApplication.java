b2bfac6540e3d4045228ef7713378d498a7cd058
dengtao <981376577@qq.com>
2024-09-29 11:05:35 +0800

    build: 修改yml dingtalk配置文件

--- taotao-cloud-microservice/taotao-cloud-shell/src/main/java/com/taotao/cloud/shell/TaoTaoCloudShellApplication.java
+++ taotao-cloud-microservice/taotao-cloud-shell/src/main/java/com/taotao/cloud/shell/TaoTaoCloudShellApplication.java
@@ -45,13 +45,19 @@ public class TaoTaoCloudShellApplication {
     public CorsFilter corsFilter() {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true); // 允许cookies跨域
-        config.addAllowedOriginPattern("*"); // #允许向该服务器提交请求的URI，*表示全部允许，在SpringMVC中，如果设成*，会自动转成当前请求头中的Origin
-        config.addAllowedHeader("*"); // #允许访问的头信息,*表示全部
-        config.setMaxAge(18000L); // 预检请求的缓存时间（秒），即在这个时间段里，对于相同的跨域请求不会再预检了
-        config.addAllowedMethod("OPTIONS"); // 允许提交请求的方法，*表示全部允许
+		// 允许cookies跨域
+        config.setAllowCredentials(true);
+		// #允许向该服务器提交请求的URI，*表示全部允许，在SpringMVC中，如果设成*，会自动转成当前请求头中的Origin
+        config.addAllowedOriginPattern("*");
+		// #允许访问的头信息,*表示全部
+        config.addAllowedHeader("*");
+		// 预检请求的缓存时间（秒），即在这个时间段里，对于相同的跨域请求不会再预检了
+        config.setMaxAge(18000L);
+		// 允许提交请求的方法，*表示全部允许
+        config.addAllowedMethod("OPTIONS");
         config.addAllowedMethod("HEAD");
-        config.addAllowedMethod("GET"); // 允许Get的请求方法
+		// 允许Get的请求方法
+        config.addAllowedMethod("GET");
         config.addAllowedMethod("PUT");
         config.addAllowedMethod("POST");
         config.addAllowedMethod("DELETE");
