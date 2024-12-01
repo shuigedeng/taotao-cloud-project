085bfa7b383207a9cde467505270a129c94d115f
shuigedeng <981376577@qq.com>
2024-12-01 17:25:03 +0800

    build: 升级2025.01

--- doc/scripts/taotao-cloud-monitor-start.sh
+++ doc/scripts/taotao-cloud-monitor-start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-docker pull registry.cn-hangzhou.aliyuncs.com/taotao-cloud-project/taotao-cloud-monitor:2024.12
+docker pull registry.cn-hangzhou.aliyuncs.com/taotao-cloud-project/taotao-cloud-monitor:2025.01
 
-docker tag registry.cn-hangzhou.aliyuncs.com/taotao-cloud-project/taotao-cloud-monitor:2024.12 taotao-cloud-monitor:latest
+docker tag registry.cn-hangzhou.aliyuncs.com/taotao-cloud-project/taotao-cloud-monitor:2025.01 taotao-cloud-monitor:latest
 
 docker stop monitor
 
