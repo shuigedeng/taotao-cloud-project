9df220b7f91d9165e1fc1dcacd841e2cf919faa6
shuigedeng <981376577@qq.com>
2025-06-03 16:54:03 +0800

    开启2025.06

--- doc/scripts/taotao-cloud-gateway-start.sh
+++ doc/scripts/taotao-cloud-gateway-start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-docker pull registry.cn-hangzhou.aliyuncs.com/taotao-cloud-project/taotao-cloud-gateway:2025.06
+docker pull registry.cn-hangzhou.aliyuncs.com/taotao-cloud-project/taotao-cloud-gateway:2025.07
 
-docker tag registry.cn-hangzhou.aliyuncs.com/taotao-cloud-project/taotao-cloud-gateway:2025.06 taotao-cloud-gateway:latest
+docker tag registry.cn-hangzhou.aliyuncs.com/taotao-cloud-project/taotao-cloud-gateway:2025.07 taotao-cloud-gateway:latest
 
 docker stop gateway
 
--- doc/scripts/taotao-cloud-monitor-start.sh
+++ doc/scripts/taotao-cloud-monitor-start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-docker pull registry.cn-hangzhou.aliyuncs.com/taotao-cloud-project/taotao-cloud-monitor:2025.06
+docker pull registry.cn-hangzhou.aliyuncs.com/taotao-cloud-project/taotao-cloud-monitor:2025.07
 
-docker tag registry.cn-hangzhou.aliyuncs.com/taotao-cloud-project/taotao-cloud-monitor:2025.06 taotao-cloud-monitor:latest
+docker tag registry.cn-hangzhou.aliyuncs.com/taotao-cloud-project/taotao-cloud-monitor:2025.07 taotao-cloud-monitor:latest
 
 docker stop monitor
 
