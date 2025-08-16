f4395a81acd293712b2aee62146b7d5076fa19b3
shuigedeng <981376577@qq.com>
2025-08-16 19:59:45 +0800

    开启2025.09

--- taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-auth/taotao-cloud-auth-sa/src/main/java/com/taotao/cloud/sa/just/biz/just/justauth/entity/JustAuthConfig.java
+++ taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-auth/taotao-cloud-auth-sa/src/main/java/com/taotao/cloud/sa/just/biz/just/justauth/entity/JustAuthConfig.java
@@ -20,7 +20,7 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import com.taotao.boot.data.mybatis.base.entity.MpSuperEntity;
+import com.taotao.boot.data.mybatis.mybatisplus.base.entity.MpSuperEntity;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.experimental.*;
--- taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-customer/taotao-cloud-customer-biz/src/main/java/com/taotao/cloud/customer/biz/model/entity/Chatbot.java
+++ taotao-cloud-microservice/taotao-cloud-business/taotao-cloud-customer/taotao-cloud-customer-biz/src/main/java/com/taotao/cloud/customer/biz/model/entity/Chatbot.java
@@ -20,11 +20,11 @@ import com.taotao.boot.data.jpa.base.entity.JpaSuperEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import lombok.*;
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.Accessors;
 
 /**
  * 机器人客服表
@@ -35,7 +35,7 @@ import lombok.Setter;
  */
 @Setter
 @Getter
-@Accessors(chain=true)
+@Accessors(chain = true)
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
@@ -43,45 +43,61 @@ import lombok.Setter;
 @org.springframework.data.relational.core.mapping.Table(name = "tt_chatbot", comment = "机器人客服表")
 public class Chatbot extends JpaSuperEntity {
 
-    private static final long serialVersionUID = 6887296988458221221L;
+	private static final long serialVersionUID = 6887296988458221221L;
 
-    /** 机器人名称 */
-    @Column(name = "name", columnDefinition = "varchar(32) not null comment '机器人名称'")
-    private String name;
+	/**
+	 * 机器人名称
+	 */
+	@Column(name = "name", columnDefinition = "varchar(32) not null comment '机器人名称'")
+	private String name;
 
-    /** 基础url */
-    @Column(name = "base_url", columnDefinition = "varchar(255) not null comment '基础url'")
-    private String baseUrl;
+	/**
+	 * 基础url
+	 */
+	@Column(name = "base_url", columnDefinition = "varchar(255) not null comment '基础url'")
+	private String baseUrl;
 
-    /** 首选语言 */
-    @Column(name = "primary_language", columnDefinition = "varchar(20) not null comment '首选语言'")
-    private String primaryLanguage;
+	/**
+	 * 首选语言
+	 */
+	@Column(name = "primary_language", columnDefinition = "varchar(20) not null comment '首选语言'")
+	private String primaryLanguage;
 
-    /** 兜底回复 */
-    @Column(name = "fallback", columnDefinition = "varchar(255) not null comment '兜底回复'")
-    private String fallback;
+	/**
+	 * 兜底回复
+	 */
+	@Column(name = "fallback", columnDefinition = "varchar(255) not null comment '兜底回复'")
+	private String fallback;
 
-    /** 欢迎语 */
-    @Column(name = "welcome", columnDefinition = "varchar(255) not null comment '欢迎语'")
-    private String welcome;
+	/**
+	 * 欢迎语
+	 */
+	@Column(name = "welcome", columnDefinition = "varchar(255) not null comment '欢迎语'")
+	private String welcome;
 
-    /** 渠道类型 */
-    @Column(name = "channel", columnDefinition = "varchar(32) not null comment '渠道类型'")
-    private String channel;
+	/**
+	 * 渠道类型
+	 */
+	@Column(name = "channel", columnDefinition = "varchar(32) not null comment '渠道类型'")
+	private String channel;
 
-    /** 渠道标识 */
-    @Column(name = "channel_mark", columnDefinition = "varchar(255) not null comment '渠道标识'")
-    private String channelMark;
+	/**
+	 * 渠道标识
+	 */
+	@Column(name = "channel_mark", columnDefinition = "varchar(255) not null comment '渠道标识'")
+	private String channelMark;
 
-    /** 是否开启 0-未开启，1-开启 */
-    @Column(name = "enabled", columnDefinition = "tinyint(1) NOT NULL DEFAULT 0 comment '是否开启 0-未开启 1-开启'")
-    private boolean enabled;
+	/**
+	 * 是否开启 0-未开启，1-开启
+	 */
+	@Column(name = "enabled", columnDefinition = "tinyint(1) NOT NULL DEFAULT 0 comment '是否开启 0-未开启 1-开启'")
+	private boolean enabled;
 
-    /**
-     * 工作模式
-     *
-     * @see WorkModeEnum
-     */
-    @Column(name = "work_mode", columnDefinition = "int not null default 0 comment '工作模式'")
-    private int workMode;
+	/**
+	 * 工作模式
+	 *
+	 * @see WorkModeEnum
+	 */
+	@Column(name = "work_mode", columnDefinition = "int not null default 0 comment '工作模式'")
+	private int workMode;
 }
