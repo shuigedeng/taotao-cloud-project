7369538874cb6752d04e0ea5a29d0993dd72ce7f
shuigedeng <981376577@qq.com>
2025-11-10 20:33:53 +0800

    开启2025.12

--- taotao-cloud-microservice/taotao-cloud-monitor/src/main/java/com/taotao/cloud/monitor/TaoTaoCloudMonitorApplication.java
+++ taotao-cloud-microservice/taotao-cloud-monitor/src/main/java/com/taotao/cloud/monitor/TaoTaoCloudMonitorApplication.java
@@ -42,7 +42,7 @@ public class TaoTaoCloudMonitorApplication {
 
         new StartupSpringApplication(TaoTaoCloudMonitorApplication.class)
                 .setTtcBanner()
-                .setTtcProfileIfNotExists("dev")
+//                .setTtcProfileIfNotExists("dev")
                 .setTtcApplicationProperty("taotao-cloud-monitor")
                 .setTtcAllowBeanDefinitionOverriding(true)
                 .run(args);
