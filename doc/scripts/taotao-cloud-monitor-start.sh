d3c0bc7cbbe24b3b64230c34c424df1092a28793
shuigedeng <981376577@qq.com>
2026-04-30 20:26:26 +0800

    开启2026.06

--- doc/scripts/taotao-cloud-monitor-start.sh
+++ doc/scripts/taotao-cloud-monitor-start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-docker pull registry.cn-hangzhou.aliyuncs.com/taotao-cloud-project/taotao-cloud-monitor:2026.05
+docker pull registry.cn-hangzhou.aliyuncs.com/taotao-cloud-project/taotao-cloud-monitor:2026.06
 
-docker tag registry.cn-hangzhou.aliyuncs.com/taotao-cloud-project/taotao-cloud-monitor:2026.05 taotao-cloud-monitor:latest
+docker tag registry.cn-hangzhou.aliyuncs.com/taotao-cloud-project/taotao-cloud-monitor:2026.06 taotao-cloud-monitor:latest
 
 docker stop monitor
 
